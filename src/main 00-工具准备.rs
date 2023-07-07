f5ae7a8e7f6b35ee51d697cab9d4cdcd7965ca3a
azin-cn <Mr.MF@foxmail.com>
2023-07-08 00:04:44 +0800

    feat: add unit0

---
+++ src/main 00-工具准备.rs
@@ -0,0 +1,15 @@
+fn main() {
+    /**
+     * vscode插件
+     *   rust-analyzer
+     *   cargo
+     *   crates
+     *   even Better TOML
+     *   Rust Syntax
+     * 具体的安装流程参考官网或其他博客文章
+     * 
+     * 学习资料：
+     *   - https://rustwiki.org/zh-CN/book/title-page.html
+     *   - https://course.rs/about-book.html
+     */
+}
