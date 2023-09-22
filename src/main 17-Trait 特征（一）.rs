2a08bb8e33fac3644cf64e99f9d221e4d35ddbb5
Mr.MF <Mr.MF@foxmail.com>
2023-09-23 00:32:23 +0800

    feat: unit 17-特征（一）

---
+++ src/main 17-Trait 特征（一）.rs
@@ -0,0 +1,55 @@
+fn main() {
+    /*
+     * ## Trait /treɪt/ 和 Trait Object 特征
+     *
+     * Trait 定义了一组可以被共享的行为，只要实现了特征，你就能使用这组行为
+     *
+     * ### Trait 和 interface
+     *
+     * 从多种数据类型中抽取出这些类型之间可通用的方法或属性，
+     * 并将它们放进另一个相对更抽象的类型中，是一种很好的代码复用方式，也是多态的一种体现方式。
+     *
+     * 在面向对象语言中，这种功能一般通过接口(interface)实现。
+     * 在Rust中，这种功能通过Trait实现。Trait类似于其他语言中接口的概念。
+     * 例如，Trait可以被其他具体的类型实现(implement)，也可以在Trait中定义一些方法，实现该Trait的类型都必须实现这些方法。
+     *
+     * 严格来说，Rust中Trait的作用主要体现在两方面：
+     * - Trait类型：用于定义抽象行为，抽取那些共性的属性，主要表现是作为泛型的数据类型(对泛型进行限制)，类似 `String` 作为类型限制数据类型。
+     * - Trait对象：即Trait Object，能用于多态。类似其他语言中的接口（是实现接口，不是继承父类，没有继承的概念，Rust更强调组合）。组合是灵活且易读的，当继承链非常长时，难以判断属性是继承于谁
+     *
+     * 总之，Trait很重要，说是Rust的基石也不为过，它贯穿于整个Rust。
+     */
+
+    trait Playable {
+        fn play(&self);
+        fn pause(&self) {
+            println!("pause");
+        }
+        fn get_duration(&self) -> f32;
+    }
+
+    struct Audio {
+        name: String,
+        duration: f32,
+    }
+
+    // 为 Audio 实现/组合 Playable Trait
+    impl Playable for Audio {
+        fn play(&self) {
+            println!("playing");
+        }
+        fn get_duration(&self) -> f32 {
+            self.duration
+        }
+    }
+
+    // 组合是灵活且易读的，当继承链非常长时，难以判断属性是继承于谁
+    println!(
+        "{}",
+        Audio {
+            name: String::from("Hello"),
+            duration: 12.0
+        }
+        .get_duration()
+    );
+}
