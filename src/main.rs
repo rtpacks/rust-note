ce46a848a1c2f50bf4d6ac055721c8c6f0d65003
Mr.MF <Mr.MF@foxmail.com>
2023-11-12 23:33:24 +0800

    feat: unit 32-使用use及受限可见性

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,21 @@ use std::io::{self, Read};
 fn main() {
     /*
      * ## 使用 use 及受限可见性
-     * 
+     * 在同一个包内 rust 可以通过绝对路径 `crate::front_of_house::hosting` 或相对路径 `front_of_house::hosting::add_to_waitlist` 直接使用模块。
+     * 如果需要在其他包中使用，需要使用 `use` 关键字导入模块或模块内的函数、结构体等内容，导入的路径也是绝对路径和相对路径两种：
+     * - 绝对路径 `use crate::front_of_house::hosting;`
+     * - 相对路径 `use front_of_house::hosting;`
+     *
+     * 导入之后就可以使用模块或模块内容：
+     *
+     *```sh
+     * src
+     *  │─ lib.rs
+     *  │─ front_of_house.rs // 文件夹不完全管理模块的形式
+     *  └─ front_of_house
+     *      │─ hosting.rs
+     *      └─ serving.rs
+     * ```
+     *
      */
 }
