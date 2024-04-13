38b9446e86b1c5e6ab5e172eb3279888136181f7
Mr.MF <Mr.MF@foxmail.com>
2024-04-13 20:55:51 +0800

    feat: unit 46-类型转换（一）数值类型转换

--- src/main.rs
+++ src/main.rs
@@ -1,265 +1,139 @@
 use ilearn::{run, Config};
-use std::{array::IntoIter, collections::HashMap, env, error::Error, fmt::Display, fs, process};
+use std::{
+    array::IntoIter, collections::HashMap, convert::TryInto, env, error::Error, fmt::Display, fs,
+    mem::size_of, process,
+};
 
 fn main() {
     /*
-     * ## 迭代器
+     * ## 类型转换
+     * Rust 是类型安全的语言，因此在 Rust 中做类型转换不是一件简单的事。
      *
-     * rust中，**迭代器的方法**可以细分为消费者适配器（consuming adaptors）和迭代器适配器（iterator adaptors），两者的区别在于是否消费迭代器，即是否调用迭代器的 next 方法。
-     *
-     * ### 消费者适配器
-     * 消费者适配器（consuming adaptors）是迭代器上的方法，它会消费掉迭代器和迭代器中的元素，然后返回其类型的值，因此被称为消费。
-     * 这些消费者（方法）都有一个共同的特点：在它们的定义中，都依赖 next 方法来消费元素。这也是为什么迭代器要实现 Iterator 特征时必须要实现 next 方法的原因。
-     *
-     * 只要迭代器上的某个方法 A 在其内部调用了 next 方法，那么 A 就可以被称为消费性适配器。这是因为 next 方法会消耗掉迭代器上的元素，所以方法 A 的调用也会消耗掉迭代器上的元素。
-     *
-     * 其中一个例子是 sum 方法，它会拿走迭代器的所有权，然后通过不断调用 next 方法对里面的元素进行求和：
+     * 首先看一段错误的实例，a 和 b 拥有不同的类型，**Rust 不允许两种不同的类型进行比较**：
      * ```rust
-     * let v = vec![1, 2, 3];
-     * let iter = v.iter();
-     * let total: i32 = iter.sum(); // 消费者适配器需要标注数据类型
-     * // println!("{:#?}", iter); 不能再访问iter，因为sum消费了迭代器和迭代器中的元素
-     * println!("{total}");
+     * let a: i32 = 10;
+     * let b: u16 = 100;
+     *
+     * if a < b { println!("a < b 执行"); } 代码报错，因为a和b是不同的类型，不能直接用于比较
+     * ```
+     * 解决方法很简单，把数据类型改成同一种就可以通过编译。针对数值类型（包括字符ASCII），rust提供了两种**专属数值类型的转换方式 `as` 和 `TryInto`**
+     *
+     * ### as 转换
+     *
+     * as 转换非常方便，但使用时需要小心。因为每个数值类型能表达的数据范围不同，如果把范围较大的类型转换成较小的类型，可能会造成数据溢出的错误。
+     *
+     * 因此**尽量把范围较小的类型转换成较大的类型**，来避免这些问题的发生。数值类型可以通过 `i8::MAX` 的形式查看该类型能表达的最大值。
+     *
+     * #### 数值与数值
+     * ```rust
+     * let a: i32 = 10;
+     * let b: u16 = 100;
+     * let transformed_a = a as u16;
+     * let transformed_b = b as i32;
+     * println!("{transformed_a}, {transformed_b}");
      * ```
      *
-     * 可以看到sum函数的定义 `fn sum(self) {}`，拿走了迭代器的所有权：
+     * #### 数值与字符
+     * 转换字符，只有 `u8` 类型的数据（ASCII范围）才能转换成字符，但字符可以转换成包含u8范围的数值类型，如i16，u16等
      * ```rust
-     * fn sum<S>(self) -> S
-     * where
-     *     Self: Sized,
-     *     S: Sum<Self::Item>,
-     * {
-     *     Sum::sum(self)
+     * let a = 'a';
+     * let transformed_a = a as u8;
+     * let c: u8 = 99;
+     * let transformed_c = c as char;
+     * println!("{transformed_a}, {transformed_c}");
+     * ```
+     *
+     * #### 数值与内存指针
+     *
+     * rust 指针可通过 `as_mut_ptr` 获取，它的形式与 `C` 一样，数组的指针也是第一个元素的指针。
+     *
+     * 指针常用 usize（4 bytes(32 bit) 或 8 bytes(64 bit)）表示。
+     *
+     * > 数组指针偏移，第N+1元素地址 = 第 N 元素地址 + 元素内存大小
+     *
+     * ```rust
+     * let mut v = vec![0; 3];
+     * let p1: *mut i32 = v.as_mut_ptr();
+     * let first_address = p1 as usize;
+     * let second_address = first_address + size_of::<i32>(); // 数组指针偏移，第N+1元素地址 = 第 N 元素地址 + 元素大小
+     * let p2 = second_address as *mut i32;
+     * unsafe {
+     *     *p2 += 1;
      * }
+     * println!(
+     *     "v = {:?}, second_address = {:?} p2 = {:?}",
+     *     v, second_address, p2
+     * );
      * ```
      *
-     * ### 迭代器适配器
-     * 迭代器适配器（iterator adapters）即迭代器方法会返回一个新的迭代器，这是实现链式方法调用的关键：`v.iter().map().filter()...`。
-     * 与消费者适配器不同，迭代器适配器是惰性的，意味着需要一个**消费者适配器来收尾**，最终将迭代器转换成一个具体的值：
+     * #### 强制类型转换的边角知识
+     * - 转换不具有传递性 就算 e as U1 as U2 是合法的，也不能说明 e as U2 是合法的（e 不能直接转换成 U2），因为as关键字执行的转换可能涉及到具体的转换逻辑和额外的步骤，这些逻辑在不同的转换路径中可能不适用。
+     *
+     * ### TryInto 特征
+     * 在一些场景中，使用 as 关键字会有比较大的限制，比如转换失败直接panic。如果希望在类型转换上拥有完全的控制而不依赖内置的转换，例如处理转换错误，那么可以使用 TryInto 特征。
+     *
+     * **如果你要使用一个特征的方法，那么你需要引入该特征到当前的作用域中**，如果希望使用 try_into 方法，需要引入对应的 TryInto 特征。
+     * > 在1.75.0，TryInto 已通过 `std::prelude` 预导入，无需手动 use 手动导入。
+     *
+     * try_into 方法会尝试进行一次转换，并返回一个 Result，此时就可以对其进行相应的错误处理（使用时需要给结果标注转换后的类型）。
+     * 
      * ```rust
-     * let v: Vec<i32> = vec![1, 2, 3];
-     * // v.iter().map(|x| x + 1); 仅有迭代器适配器是不行的，需要消费者适配器收尾
-     * let newV: Vec<_> = v.iter().map(|x| x + 1).collect(); // 正常
+     * use std::convert::TryInto;
+     * let a = 8 as i32;
+     * let b: u8 = a.try_into().expect("转换失败");
+     * let c: u8 = match a.try_into() {
+     *     Ok(v) => v,
+     *     Err(e) => panic!("expr"),
+     * };
+     * println!("{b}, {c}");
      * ```
-     *
-     * > 为什么要区分消费者适配器和迭代器适配器两种方法呢？
-     * >
-     * > Rust语言在设计上非常注重内存安全和效率，这种设计哲学体现在它对迭代器模式的处理上。Rust区分消费性适配器（consuming adaptors）和迭代器适配器（iterator adaptors）主要是为了提供更细粒度的控制以及更明确的语义。
-     * >
-     * > 消费性适配器（Consuming Adaptors）
-     * >
-     * > 消费性适配器是那些会消耗迭代器的方法，它们会遍历迭代器并返回一个最终的结果。这意味着一旦调用了消费性适配器，原来的迭代器就不能再使用了。在Rust中，collect()就是一个消费性适配器的例子，它可以将迭代器中的元素收集到一个集合类型中，比如Vec、HashMap等。
-     * >
-     * > 迭代器适配器（Iterator Adaptors）
-     * >
-     * > 迭代器适配器则是对迭代器进行转换，但不会立即进行任何遍历操作。它们返回的是一个新的迭代器，这个新迭代器会在每次遍历时应用某种操作。在Rust中，map()就是一个迭代器适配器的例子，它会创建一个新的迭代器，这个迭代器会在每次访问时应用一个函数到原迭代器的每个元素上。
-     * >
-     * > 1. 性能优化：Rust的迭代器设计允许编译器在编译时进行更多的优化，比如通过迭代器链的懒惰求值来减少中间集合的创建，这可以显著提高程序的性能。
-     * > 2. 内存管理：Rust通过所有权系统来保证内存安全，区分消费性适配器和迭代器适配器有助于明确所有权和借用的规则，避免悬垂指针和数据竞争等问题。
-     * > 3. 明确的语义：在Rust中，当你使用collect()时，你明确地表达了你想要从迭代器中消费所有元素并生成一个集合。这种明确性有助于代码的可读性和维护性。
-     *
-     * ### collect 方法
-     * 在上面的案例中使用了一个非常强大的 collect 方法，该方法就是一个消费者适配器，它可以将一个迭代器中的元素**收集到指定类型中**。
-     *
-     * 如为收集变量标注 `Vec<_>` 类型，是为了告诉 collect：把迭代器中的元素消费掉，然后把值收集成 `Vec<_>` 类型，至于为何使用 _，因为编译器会帮我们自动推导。
-     *
-     * collect 在消费时必须显式的指定想要收集成的集合类型，是因为该方法可以收集成多种不同的集合类型，如 Vec 和 HashMap。
-     *
-     * ```rust
-     * use std::collections::HashMap;
-     *
-     * // 收集成Vec
-     *
-     * let v = vec![1, 2, 3];
-     * let newV: Vec<_> = v.iter().map(|x| x + 1).collect();
-     * println!("{:?}", newV);
-     *
-     * // 收集成HashMap
-     * let names = ["sunface", "sunfei"];
-     * let ages = [18, 18];
-     * let map: HashMap<_, _> = names.into_iter().zip(ages.into_iter()).collect();
-     * println!("{:?}", map);
-     * ```
-     * zip 是一个迭代器适配器，它的作用就是将两个迭代器的内容压缩到一起，形成 `Iterator<Item=(ValueFromA, ValueFromB)>` 这样的新的迭代器，在此处就是形如 `[(name1, age1), (name2, age2)]` 的迭代器，可以类比 JavaScript 中的 Entries 类型。
-     *
-     * 然后再通过 collect 将新迭代器中 `(K, V)` 形式的值收集成 `HashMap<K, V>`，同样的，这里必须显式声明类型，然后 HashMap 内部的 KV 类型可以交给编译器去推导，最终编译器会推导出 `HashMap<&str, i32>`。
-     *
-     * ### 闭包作为适配器参数
-     * 之前的 map 方法中，使用闭包来作为迭代器适配器的参数，它最大的好处不仅在于可以就地实现迭代器中元素的处理，还在于可以**捕获环境值**
-     * ```rust
-     * let mut index = 0;
-     * let v: Vec<_> = vec![0; 10]
-     *     .into_iter()
-     *     .map(|x| {
-     *         index += 1;
-     *         index
-     *     })
-     *     .collect();
-     *
-     * let level = 8;
-     * let standards: Vec<_> = v.into_iter().filter(|x| *x >= level).collect(); // 捕获环境变量
-     * println!("{:?}", standards)
-     * ```
-     *
-     * filter 是迭代器适配器，用于对迭代器中的每个值进行审计，符合条件则保留，反之则剔除。最后通过 collect 收集为 `Vec<i32>` 类型。
-     *
-     * ### 实现 Iterator 特征
-     * Iterator 特征不仅仅局限于 `vec` 等内置数据类型，还可以为自定义类型实现 Iterator 特征（要求实现 next 方法），使自定义类型变为迭代器。注意，是将自定义类型变成迭代器（Iterator），而不是可迭代对象（IntoIterator）。
-     *
-     * 创建一个 counter struct：
-     * ```rust
-     * struct Counter { count: i32 }
-     * impl Counter {
-     *     fn new() -> Self {
-     *         Counter { count: 0 }
-     *     }
-     * }
-     * ```
-     *
-     * 为 Counter 实现 Iterator 特征：
-     * ```rust
-     * impl Iterator for Counter {
-     *      type Item: i32;
-     *      fn next(&mut self) -> Option<Self::Item> {
-     *          if self.count < 5 {
-     *              self.count += 1;
-     *              Some(self.count)
-     *          } else {
-     *              None
-     *          }
-     *      }
-     * }
-     * ```
-     *
-     * 测试 Counter 迭代器：
-     * ```rust
-     * let total: i32 = Counter::new()
-     *     .zip(Counter::new().skip(1))
-     *     .map(|(x, y)| x + y)
-     *     .sum();
-     * println!("{:?}", total);
-     *
-     * let vector: Vec<_> = Counter::new().zip(Counter::new().skip(1)).filter(|x| *x > 0).collect();
-     * println!("{:?}", vector);
-     *
-     * let vector: Vec<_> = Counter::new().skip(3).collect();
-     * println!("{:?}", vector);
-     *
-     * // turbo fish 语法
-     * let vector = Counter::new().skip(3).collect::<Vec<i32>>()();
-     * println!("{:?}", vector);
-     *
-     * let total = Counter::new().skip(3).sum::<i32>();
-     * println!("{:?}", total);
-     * ```
-     *
-     * - skip 是一个迭代器适配器，它的作用是跳过迭代器中的前n个元素，然后返回一个新的迭代器。
-     * - zip 是一个迭代器适配器，它的作用就是将两个迭代器的内容压缩到一起，形成 `Iterator<Item=(ValueFromA, ValueFromB)>` 也就是形如 `[(name1, age1), (name2, age2)]` 的迭代器，可以类比 JavaScript 中的 Entries 类型。两者迭代器长度不一样时，以最短长度为结束条件。
-     *
-     * > turbo fish 语法参考
-     * > turbofish 语法可以允许不在变量上标注类型，在调用函数时传递目标类型以完成类型指定。
-     * > https://www.cnblogs.com/rotk2022/p/16449651.html
-     * > https://rustwiki.org/zh-CN/edition-guide/rust-2018/trait-system/impl-trait-for-returning-complex-types-with-ease.html#%E5%8F%82%E6%95%B0%E4%BD%8D%E7%BD%AE
-     * > https://rust-lang.github.io/impl-trait-initiative/explainer/apit_turbofish.html
-     *
-     * #### 实现 Iterator 特征的其它方法
-     * **其他迭代器方法都具有基于 next 方法的默认实现**，所以无需像 next 这样手动去实现。如上面案例使用到的 `zip, map, filter, sum` 等方法。
-     *
-     * ##### enumerate
-     * enumerate 方法（迭代器适配器）是常用的迭代器方法，它能生成带有索引的迭代器，返回的结构为 `Iterator<Item=(ValueA, ValueB)>` 的迭代器，即 `(index, value)` 索引在前，值在后的结构。
-     *
-     * ```rust
-     * let v = vec![1,2,3];
-     * let index: Vec<_> = v.into_iter().enumerate().collect();
-     * println!("{:?}", index);
-     * ```
-     *
-     * ### 性能
-     *
-     * 迭代器是 Rust 的 零成本抽象（zero-cost abstractions）之一，意味着抽象并不会引入运行时开销，这与 Bjarne Stroustrup（C++ 的设计和实现者）在 Foundations of C++（2012） 中所定义的 零开销（zero-overhead）如出一辙。
-     * > In general, C++ implementations obey the zero-overhead principle: What you don’t use, you don’t pay for. And further: What you do use, you couldn’t hand code any better.
-     * > 一般来说，C++的实现遵循零开销原则：没有使用时，你不必为其买单。 更进一步说，需要使用时，你也无法写出更优的代码了。
-     * 阅读：https://course.rs/advance/functional-programing/iterator.html#%E8%BF%AD%E4%BB%A3%E5%99%A8%E7%9A%84%E6%80%A7%E8%83%BD
-     *
-     * ### 更多迭代器方法
-     * 阅读：https://course.rs/std/iterator
-     *
+     * > 巩固rust认识
+     * > rust编译的过程，会根据给定的类型生成对应的代码，给变量标注类型和给表达式指定类型的效果是一样的：
+     * > ```rust
+     * > let a = 8 as u8;
+     * > let b: u8 = 8;
+     * > ```
      *
      */
 
-    let v = vec![1, 2, 3];
-    let mut iter = v.into_iter();
-    let total: i32 = iter.sum();
-    // println!("{:#?}", iter); 不能再访问iter，因为sum消费了迭代器和迭代器中的元素
-    println!("{total}");
+    let a: i32 = 10;
+    let b: u16 = 100;
+    // if a < b { println!("a < b 执行了") } 代码报错，因为 a 和 b是不同的类型，rust不允许两种不同的类型进行比较
+    println!("{:?}", i8::MIN);
+    println!("{:?}", u8::MIN == 0);
 
-    let v = vec![1, 2, 3];
-    let mut iter = v.into_iter();
+    // 数值与数值
+    let transformed_a = a as u16;
+    let transformed_b = b as i32;
+    println!("{transformed_a}, {transformed_b}");
 
-    let total: i32 = iter.map(|x| x + 1).sum();
-    println!("{total}");
+    // 数值与字符
+    let a = 'a';
+    let transformed_a = a as u32;
+    let c: u8 = 99;
+    let transformed_c = c as char;
+    println!("{transformed_a}, {transformed_c}");
 
-    let v = vec![1, 2, 3];
-    let newV: Vec<_> = v.iter().map(|x| x + 1).collect();
-    println!("{:?}", newV);
-    let names = ["sunface", "sunfei"];
-    let ages = [18, 18];
-    let map: HashMap<_, _> = names.into_iter().zip(ages.into_iter()).collect();
-    println!("{:?}", map);
-
-    let mut index = 0;
-    let v: Vec<_> = vec![0; 10]
-        .into_iter()
-        .map(|x| {
-            index += 1;
-            index
-        })
-        .collect();
-    let level = 8;
-    let standards: Vec<_> = v.into_iter().filter(|x| *x >= level).collect();
-    println!("{:?}", standards);
-
-    // 实现一个迭代器非常方便，只需要手动实现next方法，其他的迭代器方法都有基于next方法的默认实现
-    struct Counter {
-        count: i32,
+    // 数值与内存指针
+    let mut v = vec![0; 3];
+    let p1: *mut i32 = v.as_mut_ptr();
+    let first_address = p1 as usize;
+    let second_address = first_address + size_of::<i32>();
+    let p2 = second_address as *mut i32;
+    unsafe {
+        *p2 += 1;
     }
-    impl Counter {
-        fn new() -> Self {
-            Counter { count: 0 }
-        }
-    }
-    impl Iterator for Counter {
-        type Item = i32;
-        fn next(&mut self) -> Option<Self::Item> {
-            if self.count < 5 {
-                self.count += 1;
-                Some(self.count)
-            } else {
-                None
-            }
-        }
-    }
+    println!(
+        "v = {:?}, second_address = {:?} p2 = {:?}",
+        v, second_address, p2
+    );
 
-    let total: i32 = Counter::new()
-        .zip(Counter::new().skip(1))
-        .map(|(x, y)| x + y)
-        .sum();
-    println!("{:?}", total);
-
-    let vector: Vec<_> = Counter::new().zip(Counter::new().skip(4)).collect();
-    println!("{:?}", vector);
-
-    let vector: Vec<_> = Counter::new().skip(3).filter(|x| *x > 0).collect();
-    println!("{:?}", vector);
-
-    // turbo fish语法
-    let vector = Counter::new().skip(3).filter(|x| *x > 0).collect::<Vec<_>>();
-    println!("{:?}", vector);
-    let total = Counter::new().skip(3).filter(|x| *x > 0).sum::<i32>();
-    println!("{:?}", total);
-
-    // enumerate 生成带有索引的方法（迭代器适配器）
-    let v = vec![1, 2, 3, 4];
-    let index: Vec<_> = v.into_iter().enumerate().collect();
-    println!("{:?}", index);
+    let a = 8 as i32;
+    let b_1: u8 = a.try_into().unwrap();
+    let b: u8 = a.try_into().expect("转换失败");
+    let c: u8 = match a.try_into() {
+        Ok(v) => v,
+        Err(e) => panic!("expr"),
+    };
+    println!("{b}, {c}");
 }
