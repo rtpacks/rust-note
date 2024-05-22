b86e2f5bfc05313fd365599a70989ae9dda5e5c3
Mr.MF <Mr.MF@foxmail.com>
2024-05-22 13:29:21 +0800

    feat: unit-57循环引用与自引用

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    borrow::Borrow,
     cell::{Cell, RefCell},
     rc::{Rc, Weak},
 };
@@ -60,20 +61,12 @@ fn main() {
      *     }
      * }
      *
-     * // - 创建 a，创建 b，利用 `Rc::clone(&a)` 让 b 的 next 指向 a，即 b 引用了 a
-     * // - 利用 `Rc::clone(&b)` 让 a 的 next 指向 b，即 a 引用了 b
+     * // - 创建 a，创建 b，利用 `Rc::clone(&b)` 让 a 的 next 指向 b，即 a 引用了 b
+     * // - 利用 `Rc::clone(&a)` 让 b 的 next 指向 a，即 b 引用了 a
      * let a = Rc::new(List::Cons(1, RefCell::new(Rc::new(List::Nil))));
      * let b = Rc::new(List::Cons(2, RefCell::new(Rc::new(List::Nil))));
      * println!(
      *     "Initial: a.strone_count = {}, b.strone_count = {}",
-     *     Rc::strong_count(&a),
-     *     Rc::strong_count(&b)
-     * );
-     * if let Some(next) = b.tail() {
-     *     *next.borrow_mut() = Rc::clone(&a); // b 的 next 指向 a，即 b 引用了 a
-     * }
-     * println!(
-     *     "b.next: a.strone_count = {}, b.strone_count = {}",
      *     Rc::strong_count(&a),
      *     Rc::strong_count(&b)
      * );
@@ -82,6 +75,14 @@ fn main() {
      * }
      * println!(
      *     "a.next: a.strone_count = {}, b.strone_count = {}",
+     *     Rc::strong_count(&a),
+     *     Rc::strong_count(&b)
+     * );
+     * if let Some(next) = b.tail() {
+     *     *next.borrow_mut() = Rc::clone(&a); // b 的 next 指向 a，即 b 引用了 a
+     * }
+     * println!(
+     *     "b.next: a.strone_count = {}, b.strone_count = {}",
      *     Rc::strong_count(&a),
      *     Rc::strong_count(&b)
      * );
@@ -100,20 +101,162 @@ fn main() {
      * `a.tail` 调用返回 `a.next` 引用即 `b` 智能指针，打印 `b` 智能指针时会打印 `b.next` 智能指针（打印一个完整的链表数据）。
      * `b.next` 即 `a`，打印 `a` 时同样因为要打印完整的数据，会打印 `a.next` 即 `b`，最后出现循环引用不断解析引用指向的数据。
      * Rust 试图打印出 `a -> b -> a ···` 的所有内容，最终 main 线程终于不堪重负，发生了栈溢出。
-     * 
+     *
      * 创建循环引用并不简单，但是也并不是完全遇不到，当使用 `RefCell<Rc<T>>` 或者**具备内部可变性和引用计数的嵌套组合类型**时，就要打起万分精神，避免出现循环引用。
-     * 
+     *
      * ### Weak
+     * 在上面的循环引用案例中，main 函数结束前释放变量，由于值被其他变量引用所以不能被释放。这种引用属于强引用，只要存在强引用，值就不会被释放，换句话说强引用会阻止值的释放。
+     *
+     * 相比较强引用，**弱引用不持有值所有权，不保证引用关系一直有效，它仅仅保存一份指向数据的弱引用**。
+     * 所以所引用它无法阻止所引用的内存值被释放，即**弱引用本身不对值的存在性做任何担保**，引用的值还存在就返回 Some，不存在就返回 None。
+     *
+     * `Weak` 就是一个弱引用，它与 Rc 非常相似，但是与 Rc 持有所有权不同，Weak 不持有所有权，它仅仅保存一份指向数据的弱引用。
+     * `Weak` 虽然与 Rc 智能指针不同，但是有效 `Weak` 的生成总是需要 Rc 指针，通过值的 Rc 指针形式 `Rc::downgrade` 生成有效的 Weak 指针。
+     *
+     * 如果想要访问数据，需要通过 Weak 指针的 `upgrade` 方法实现，该方法返回一个类型为 `Option<Rc<T>>` 的值。
+     *
+     * **对比 Weak 和 Rc**
+     * | Weak 弱引用，不计数                         | Rc 强引用，引用计数                    |
+     * | ----------------------------------------- | ------------------------------------- |
+     * | 不拥有所有权                               | 拥有访问值的能力（不可变引用访问）        |
+     * | 不阻止值被释放(drop)                        | 所有权计数归零，才能 drop               |
+     * | 引用的值存在返回 Some，不存在返回 None       | 引用的值必定存在                        |
+     * | 通过 upgrade 取到 `Option<Rc<T>>`，然后再取值 | 通过 Deref 自动解引用，取值无需任何操作 |
+     *
+     * Weak 的功能非常弱，而这种弱恰恰适合以下的场景：
+     * - 阻止 Rc 导致的循环引用，因为 Rc 智能指针的引用计数机制，可能会导致一个值对应的多个 Rc 计数无法归零
+     * - 希望持有一个 Rc 类型的对象的临时引用，并希望这个引用不影响任何值的释放
+     *
+     * 在上面的 List 结构中，可以使用这种方式来解决循环引用问题：让前一个节点通过 Rc 来引用后一个节点，然后让后一个节点通过 Weak 来引用前一个节点。
+     * Rc 强引用意味着数据不会被释放，Weak 需要使用 `upgrade` 后才能访问数据，意味着无法直接访问弱引用指向的数据，能避开循环引用的陷阱。
+     *
+     * ```rust
+     * // 由于多出一种 Weak 类型，当前的List不满足使用，需要拓展字段。同时有可能需要修改前一个节点的值，所以用 `RefCell` 包裹
+     * // 利用枚举创建一个空值类型，避免结构体没有空值的问题
+     * #[derive(Debug)]
+     * enum Node {
+     *     // value, prev, next
+     *     Cons(i32, RefCell<Weak<Node>>, RefCell<Rc<Node>>),
+     *     Nil,
+     * }
+     *
+     * impl Node {
+     *     fn next(&self) -> Option<&RefCell<Rc<Node>>> {
+     *         match self {
+     *             Node::Cons(_, _, next) => Some(next),
+     *             Node::Nil => None,
+     *         }
+     *     }
+     *     fn prev(&self) -> Option<&RefCell<Weak<Node>>> {
+     *         match self {
+     *             Node::Cons(_, prev, _) => Some(prev),
+     *             Node::Nil => None,
+     *         }
+     *     }
+     * }
+     *
+     * //  使用 Rc 指向下一个节点，使用 Weak 指向上一个节点，避免循环引用陷阱
+     * // - 创建 a，创建 b，利用 `Rc::clone(&b)` 让 a 的 next 指向 b，即 a 引用了 b
+     * // - 利用 `Rc::downgrade(&a)` 让 b 的 prev 指向 a，即 b 弱引用了 a
+     * let a = Rc::new(Node::Cons(
+     *     1,
+     *     RefCell::new(Weak::new()),
+     *     RefCell::new(Rc::new(Node::Nil)),
+     * ));
+     * let b = Rc::new(Node::Cons(
+     *     2,
+     *     RefCell::new(Weak::new()),
+     *     RefCell::new(Rc::new(Node::Nil)),
+     * ));
+     * println!(
+     *     "Initial: a.strong_count = {}, a.weak_count = {}, b.strone_count = {}, b.weak_count = {}",
+     *     Rc::strong_count(&a),
+     *     Rc::weak_count(&a),
+     *     Rc::strong_count(&b),
+     *     Rc::weak_count(&b)
+     * );
+     * if let Some(next) = a.next() {
+     *     *next.borrow_mut() = Rc::clone(&b); // a 的 next 指向 b，即 a 引用了 b
+     * }
+     * println!(
+     *     "a.next: a.strong_count = {}, a.weak_count = {}, b.strone_count = {}, b.weak_count = {}",
+     *     Rc::strong_count(&a),
+     *     Rc::weak_count(&a),
+     *     Rc::strong_count(&b),
+     *     Rc::weak_count(&b)
+     * );
+     * if let Some(prev) = b.prev() {
+     *     *prev.borrow_mut() = Rc::downgrade(&a); // Weak 有值的引用只有通过 Rc::downgrade 生成，Weak::new 是无值的弱引用
+     * }
+     * println!(
+     *     "b.prev: a.strong_count = {}, a.weak_count = {}, b.strone_count = {}, b.weak_count = {}",
+     *     Rc::strong_count(&a),
+     *     Rc::weak_count(&a),
+     *     Rc::strong_count(&b),
+     *     Rc::weak_count(&b)
+     * );
+     *
+     * println!("{:?}", a); // Weak 需要使用 upgrade 方法后才能访问真实数据，因此 Weak 会阻断循环引用的生成
+     * ```
+     *
+     * Weak 需要使用 `upgrade` 方法后才能访问真实数据，因此 Weak 会阻断循环引用的生成。
+     *
+     * 除了树类型数据结构，还有所属关系的数据结构容易引起循环引用，比如杯子与所属人的关系，一个人可以有多个杯子，一个杯子属于一个人。
+     * 在这种设计下，可以将人属性中的杯子属性设置为 Weak 或者把杯子中的所有人属性设置为 Weak。
      * 
-     * 
-     * 
+     * ```rust
+     * #[derive(Debug)]
+     * struct Owner {
+     *     id: i32,
+     *     // RefCell<Vec<Weak<Cup>>> 比 Vec<RefCell<Weak<Cup>>> 更好的在于可以直接借用一整个vec，就可以修改vec内的元素。不需要单独借用再修改
+     *     cups: RefCell<Vec<Weak<Cup>>>,
+     * }
      *
+     * #[derive(Debug)]
+     * struct Cup {
+     *     id: i32,
+     *     owner: RefCell<Rc<Owner>>,
+     * }
      *
+     * // Rc::clone(&owner)
+     * let owner = Rc::new(Owner {
+     *     id: 0,
+     *     cups: RefCell::new(vec![]),
+     * });
+     * let cup1 = Rc::new(Cup {
+     *     id: 1,
+     *     owner: RefCell::new(Rc::clone(&owner)),
+     * });
+     * let cup2 = Rc::new(Cup {
+     *     id: 2,
+     *     owner: RefCell::new(Rc::clone(&owner)),
+     * });
      *
+     * println!(
+     *     "Initial: owner.strong_count = {}, owner.weak_count = {}, \ncup1.strong_count = {}, cup1.weak_count = {}, \ncup2.strong_count = {}, cup2.weak_count = {}",
+     *     Rc::strong_count(&owner),
+     *     Rc::weak_count(&owner),
+     *     Rc::strong_count(&cup1),
+     *     Rc::weak_count(&cup1),
+     *     Rc::strong_count(&cup2),
+     *     Rc::weak_count(&cup2),
+     * );
      *
+     * *owner.cups.borrow_mut() = vec![Rc::downgrade(&cup1), Rc::downgrade(&cup2)];
      *
+     * println!("{:?}", owner);
      *
-     *
+     * for cup in owner.cups.borrow().iter() {
+     *     match cup.upgrade() {
+     *         Some(x) => {
+     *             println!("{x:?}")
+     *         }
+     *         None => {
+     *             println!("当前引用失效")
+     *         }
+     *     }
+     * }
+     * ```
      *
      */
 
@@ -146,20 +289,12 @@ fn main() {
         }
     }
 
-    // - 创建 a，创建 b，利用 `Rc::clone(&a)` 让 b 的 next 指向 a，即 b 引用了 a
-    // - 利用 `Rc::clone(&b)` 让 a 的 next 指向 b，即 a 引用了 b
+    // - 创建 a，创建 b，利用 `Rc::clone(&b)` 让 a 的 next 指向 b，即 a 引用了 b
+    // - 利用 `Rc::clone(&a)` 让 b 的 next 指向 a，即 b 引用了 a
     let a = Rc::new(List::Cons(1, RefCell::new(Rc::new(List::Nil))));
     let b = Rc::new(List::Cons(2, RefCell::new(Rc::new(List::Nil))));
     println!(
         "Initial: a.strone_count = {}, b.strone_count = {}",
-        Rc::strong_count(&a),
-        Rc::strong_count(&b)
-    );
-    if let Some(next) = b.tail() {
-        *next.borrow_mut() = Rc::clone(&a); // b 的 next 指向 a，即 b 引用了 a
-    }
-    println!(
-        "b.next: a.strone_count = {}, b.strone_count = {}",
         Rc::strong_count(&a),
         Rc::strong_count(&b)
     );
@@ -171,9 +306,136 @@ fn main() {
         Rc::strong_count(&a),
         Rc::strong_count(&b)
     );
+    if let Some(next) = b.tail() {
+        *next.borrow_mut() = Rc::clone(&a); // b 的 next 指向 a，即 b 引用了 a
+    }
+    println!(
+        "b.next: a.strone_count = {}, b.strone_count = {}",
+        Rc::strong_count(&a),
+        Rc::strong_count(&b)
+    );
 
     // 成功创建了循环引用a-> b -> a -> b ····
 
     // 循环引用，造成8MB的主线程栈溢出
     // println!("{:?}", a.tail());
+
+    // 由于多出一种 Weak 类型，当前的List不满足使用，需要拓展字段。同时有可能需要修改前一个节点的值，所以用 `RefCell` 包裹
+    // 利用枚举创建一个空值类型，避免结构体没有空值的问题
+    #[derive(Debug)]
+    enum Node {
+        // value, prev, next
+        Cons(i32, RefCell<Weak<Node>>, RefCell<Rc<Node>>),
+        Nil,
+    }
+
+    impl Node {
+        fn next(&self) -> Option<&RefCell<Rc<Node>>> {
+            match self {
+                Node::Cons(_, _, next) => Some(next),
+                Node::Nil => None,
+            }
+        }
+        fn prev(&self) -> Option<&RefCell<Weak<Node>>> {
+            match self {
+                Node::Cons(_, prev, _) => Some(prev),
+                Node::Nil => None,
+            }
+        }
+    }
+
+    //  使用 Rc 指向下一个节点，使用 Weak 指向上一个节点，避免循环引用陷阱
+    // - 创建 a，创建 b，利用 `Rc::clone(&b)` 让 a 的 next 指向 b，即 a 引用了 b
+    // - 利用 `Rc::downgrade(&a)` 让 b 的 prev 指向 a，即 b 弱引用了 a
+    let a = Rc::new(Node::Cons(
+        1,
+        RefCell::new(Weak::new()),
+        RefCell::new(Rc::new(Node::Nil)),
+    ));
+    let b = Rc::new(Node::Cons(
+        2,
+        RefCell::new(Weak::new()),
+        RefCell::new(Rc::new(Node::Nil)),
+    ));
+    println!(
+        "Initial: a.strong_count = {}, a.weak_count = {}, b.strone_count = {}, b.weak_count = {}",
+        Rc::strong_count(&a),
+        Rc::weak_count(&a),
+        Rc::strong_count(&b),
+        Rc::weak_count(&b)
+    );
+    if let Some(next) = a.next() {
+        *next.borrow_mut() = Rc::clone(&b); // a 的 next 指向 b，即 a 引用了 b
+    }
+    println!(
+        "a.next: a.strong_count = {}, a.weak_count = {}, b.strone_count = {}, b.weak_count = {}",
+        Rc::strong_count(&a),
+        Rc::weak_count(&a),
+        Rc::strong_count(&b),
+        Rc::weak_count(&b)
+    );
+    if let Some(prev) = b.prev() {
+        *prev.borrow_mut() = Rc::downgrade(&a); // Weak 有值的引用只有通过 Rc::downgrade 生成，Weak::new 是无值的弱引用
+    }
+    println!(
+        "b.prev: a.strong_count = {}, a.weak_count = {}, b.strone_count = {}, b.weak_count = {}",
+        Rc::strong_count(&a),
+        Rc::weak_count(&a),
+        Rc::strong_count(&b),
+        Rc::weak_count(&b)
+    );
+
+    println!("{:?}", a); // Weak 需要使用 upgrade 方法后才能访问真实数据，因此 Weak 会阻断循环引用的生成
+
+    #[derive(Debug)]
+    struct Owner {
+        id: i32,
+        // RefCell<Vec<Weak<Cup>>> 比 Vec<RefCell<Weak<Cup>>> 更好的在于可以直接借用一整个vec，就可以修改vec内的元素。不需要单独借用再修改
+        cups: RefCell<Vec<Weak<Cup>>>,
+    }
+
+    #[derive(Debug)]
+    struct Cup {
+        id: i32,
+        owner: RefCell<Rc<Owner>>,
+    }
+
+    // Rc::clone(&owner)
+    let owner = Rc::new(Owner {
+        id: 0,
+        cups: RefCell::new(vec![]),
+    });
+    let cup1 = Rc::new(Cup {
+        id: 1,
+        owner: RefCell::new(Rc::clone(&owner)),
+    });
+    let cup2 = Rc::new(Cup {
+        id: 2,
+        owner: RefCell::new(Rc::clone(&owner)),
+    });
+
+    println!(
+        "Initial: owner.strong_count = {}, owner.weak_count = {}, \ncup1.strong_count = {}, cup1.weak_count = {}, \ncup2.strong_count = {}, cup2.weak_count = {}",
+        Rc::strong_count(&owner),
+        Rc::weak_count(&owner),
+        Rc::strong_count(&cup1),
+        Rc::weak_count(&cup1),
+        Rc::strong_count(&cup2),
+        Rc::weak_count(&cup2),
+    );
+
+    *owner.cups.borrow_mut() = vec![Rc::downgrade(&cup1), Rc::downgrade(&cup2)];
+
+    println!("{:?}", owner);
+
+    for cup in owner.cups.borrow().iter() {
+        match cup.upgrade() {
+            Some(x) => {
+                println!("{x:?}")
+            }
+            None => {
+                println!("当前引用失效")
+            }
+        }
+    }
 }
