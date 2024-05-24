3546a01cadf3c7b72c29b3faa8d73b138b5bdc55
Mr.MF <Mr.MF@foxmail.com>
2024-05-25 00:08:24 +0800

    feat: unit 58-结构体的自引用

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::{
     borrow::{Borrow, BorrowMut},
     cell::{Cell, RefCell},
+    marker::{self, PhantomPinned},
     os::raw,
+    pin::Pin,
     ptr,
     rc::{Rc, Weak},
 };
@@ -152,31 +154,63 @@ fn main() {
      * ### Pin 固定值的地址
      * unsafe 实现不能保证安全的原因是 value 的地址可能发生更改，`pointer_to_value` 记录 value 的原地址可能是一个未定义的数据，访问存在未定义行为的风险。
      * > 比如完全克隆一份 `selfRef` 数据，然后释放原有的 `selfRef` 数据，此时新数据的 `selfRef.pointer_to_value` 存储的原 value 地址就是一个未定义的数据，访问此时的 `selfRef.pointer_to_value` 就是未定义的行为。
-     * 
+     *
      * 如果将 value 的地址固定，`pointer_to_value: *const` 就是一直有效的地址，不会存在未定义行为的风险。rust 提供的 Pin 智能指针(结构体)提供了固定地址的功能。
      *
      * 从**是否可以在内存中安全的被移动**的角度，rust 的类型分类两类，`Unpin` 和 `!Unpin`，具体的区分如下：
-     * - `Unpin` 表示类型可以在内存中安全地移动，即能安全的改变地址不会带来意外的错误。绝大多数标准库类型都实现了 Unpin。
-     * - `!Unpin` 表示类型不可以在内存中安全的移动，即在改变地址的时会发生意外的副作用，比如裸指针实现的自引用结构体，改变结构体地址后，存储的裸指针还是访问原地址，存在未定义行为的风险。
-     * 
+     * - `Unpin` 表示类型**可以在内存中安全地移动**，即能安全的改变地址不会带来意外的错误。绝大多数标准库类型都实现了 Unpin。
+     * - `!Unpin` 表示类型**在内存中移动可能会发生意外的副作用**，比如裸指针实现的自引用结构体，改变结构体地址后，存储的裸指针还是访问原地址，存在未定义行为的风险。
+     *
      * > 特征前的 `!` 代表没有实现某个特征的意思，`!Unpin` 说明类型没有实现 Unpin 特征。
-     * 
+     *
+     * 这些定义与名称会比较绕，`Unpin` 表示**不需要被固定就可以安全移动的类型**，`!Unpin` 表示没有实现 `Unpin` 特征的类型，也就是在内存移动中可能发生副作用的类型。
+     * 为什么不用 Pin 和 Unpin？这是因为 rust 将 `Pin` 作为“动作”的智能指针（结构体），而不是特征，**`Pin` 表示固定一个值的地址**，它可以接收实现 `Unpin` 或 `!Unpin` 特征的类型。
+     * - 如果 Pin 的是 Unpin 类型，则还是可以被移动走的。因为实现 Unpin 就表示移动是安全的。
+     * - 如果 Pin 的是 !Unpin 类型，则无法被移动走。因为 !Unpin 就表示移动是不安全的。
+     *
+     * **Pin 不仅可以将值固定到堆上，也可以固定到栈上**。将 `!Unpin` 值固定到堆上无需 unsafe，可以通过 `Box::pin` 来简单的实现，将 `!Unpin` 值固定到栈上需要使用 unsafe。
+     * `Box::pin` 函数在内部处理了固定操作，通过类型系统和所有权规则来自动保证值在整个生命周期内不会被移动。栈上的固定涉及到更直接的内存操作，可能需要手动保证对象不被移动。
+     * 由于Rust的安全模型默认不允许直接操控栈上的内存位置，因此需要使用 unsafe 来明确绕过这些限制，这也意味着固定在栈上的 `!Unpin`，不仅需要使用 Pin，而且还需要 `unsafe`。
+     *
+     * 结构体实现 `Unpin` 和 `!Unpin`：
      * - 如果自定义的类型成员都实现了 Unpin 的，那么该自定义类型会自动实现 Unpin。
-     * - `!Unpin` 类型需要通过 PhantomPinned 标记，只要自定义构体里面有一个成员是 `!Unpin`，则该结构也是 `!Unpin`。
+     * - `!Unpin` 类型需要通过 PhantomPinned 特殊标记，只要自定义构体里面有一个成员是 `!Unpin`，则该结构也是 `!Unpin`。
      *
-     * // TODO 为什么值可以移动，值是怎么移动的，完成Pin
-     * Pin 是一个智能指针（结构体），`Unpin` 和 `!Unpin` 则是特征。
-     * 如果 Pin 的是 Unpin 类型，则还是可以被移动走的。因为实现 Unpin 就表示移动是安全的。
-     * 如果 Pin 的是 !Unpin 类型，则无法被移动走。因为 !Unpin 就表示移动是不安全的。
-     * 可以将值固定到栈上，也可以固定到堆上。将 !Unpin 值固定到栈上需要使用 unsafe，将 !Unpin 值固定到堆上无需 unsafe，可以通过 Box::pin 来简单的实现
+     * ```rust
+     * // 不能安全移动的类型，即在内存中移动数据可能会导致意外的副作用，通常需要 PhantomPinned 标识字段
+     * // 只要有一个字段被标示为 `!Unpin`（一般都是 PhantomPinned 作为值），那么整个结构体就实现 `!Unpin` 特征
+     * struct SelfRefNotUnpin {
+     *     value: String,
+     *     pointer_to_value: *mut String,
+     *     _pin: PhantomPinned, // 用 PhantomPinned 标识该类型是一个实现 `!Unpin` 的类型，只要一个类型属性是 `!Unpin`，这个结构体就默认成为实现 `!Unpin` 的类型
+     * }
      *
+     * let mut self_ref_not_unpin = Box::pin(SelfRefNotUnpin {
+     *     value: String::from("Hello"),
+     *     pointer_to_value: ptr::null_mut(),
+     *     _pin: PhantomPinned,
+     * });
      *
-     *
+     * // 使用Pin后，并不意味着就不需要使用 unsafe 操作，Pin 只是将由于 `!Unpin` 在内存移动可能引发副作用的移动限制了，不会移动就不发生 `!Unpin` 的副作用。
+     * // 至于在原内存空间修改值，还是需要 unsafe 操作获取被 Pin 包括的值，此时的 unsafe 代码是安全的，因为修改内部的值不会导致整体的内存地址发生变化
+     * unsafe {
+     *     // 被包裹的PinRef
+     *     let mut self_ref_not_unpin_pin_mutref = self_ref_not_unpin.as_mut();
+     *     // 真正值的 mut ref
+     *     let mut self_ref_not_unpin_mutref = Pin::get_unchecked_mut(self_ref_not_unpin_pin_mutref);
+     *     self_ref_not_unpin_mutref.pointer_to_value = &mut self_ref_not_unpin_mutref.value
+     * }
+     * ```
      * 
-     * 可以将值固定到栈上，也可以固定到堆上 将 !Unpin 值固定到栈上需要使用 unsafe 将 !Unpin 值固定到堆上无需 unsafe ，可以通过 Box::pin 来简单的实现
+     * 使用Pin后，并不意味着就不需要使用 unsafe 操作，Pin 只是将由于 `!Unpin` 在内存移动可能引发副作用的移动限制了，不会移动就不发生 `!Unpin` 的副作用。
+     * 至于在原内存空间修改值，还是需要 unsafe 操作获取被 Pin 包括的值，此时的 unsafe 代码是安全的，因为修改内部的值不会导致整体的内存地址发生变化
      *
      *
+     * ### 总结
+     * Pin 是一个智能指针（结构体），`Unpin` 和 `!Unpin` 则是特征。
+     * 
      * TODO 等某一天使用到自引用结构时再来补齐
+     *
      */
 
     #[derive(Debug)]
@@ -242,5 +276,27 @@ fn main() {
         selfRef.value, pointer_to_vlaue, selfRef.pointer_to_value
     );
 
-    
+    // 不能安全移动的类型，即在内存中移动数据可能会导致意外的副作用，通常需要 PhantomPinned 标识字段
+    // 只要有一个字段被标示为 `!Unpin`（一般都是 PhantomPinned 作为值），那么整个结构体就实现 `!Unpin` 特征
+    struct SelfRefNotUnpin {
+        value: String,
+        pointer_to_value: *mut String,
+        _pin: PhantomPinned, // 用 PhantomPinned 标识该类型是一个实现 `!Unpin` 的类型，只要一个类型属性是 `!Unpin`，这个结构体就默认成为实现 `!Unpin` 的类型
+    }
+
+    let mut self_ref_not_unpin = Box::pin(SelfRefNotUnpin {
+        value: String::from("Hello"),
+        pointer_to_value: ptr::null_mut(),
+        _pin: PhantomPinned,
+    });
+
+    // 使用Pin后，并不意味着就不需要使用 unsafe 操作，Pin 只是将由于 `!Unpin` 在内存移动可能引发副作用的移动限制了，不会移动就不发生 `!Unpin` 的副作用。
+    // 至于在原内存空间修改值，还是需要 unsafe 操作获取被 Pin 包括的值，此时的 unsafe 代码是安全的，因为修改内部的值不会导致整体的内存地址发生变化
+    unsafe {
+        // 被包裹的PinRef
+        let mut self_ref_not_unpin_pin_mutref = self_ref_not_unpin.as_mut();
+        // 真正值的 mut ref
+        let mut self_ref_not_unpin_mutref = Pin::get_unchecked_mut(self_ref_not_unpin_pin_mutref);
+        self_ref_not_unpin_mutref.pointer_to_value = &mut self_ref_not_unpin_mutref.value
+    }
 }
