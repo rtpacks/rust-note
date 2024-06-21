5db7ec64afff12ea8ada8353aa953b6d40e2e651
Mr.MF <Mr.MF@foxmail.com>
2024-06-22 00:03:38 +0800

    feat: unit 71-异步编程

--- src/main.rs
+++ src/main.rs
@@ -4,333 +4,113 @@ use std::{io, num, slice};
 fn main() {
     /*
      *
-     * ## unsafe：unsafe superpowers
+     * ## 异步编程
+     * 如果想开发 Web 服务器、数据库驱动、消息服务等需要高并发的服务，那么异步编程认真对待和学习。
      *
-     * 五种超能力（unsafe superpowers）：
-     * - 解引用裸指针
-     * - 调用不安全的函数或方法
-     * - 访问或修改可变静态变量
-     * - 实现不安全 trait
-     * - 访问 union 的字段
+     * 简单来说，异步编程是一个并发编程模型，目前主流语言基本都支持，当然，可能支持的方式有所不同。
+     * 异步编程允许同时并发运行大量的任务，却仅仅需要几个甚至一个 OS 线程或 CPU 核心，现代化的异步编程在使用体验上与同步编程几乎无区别。
+     * 例如 Go 语言的 go 关键字，也包括 async/await 语法，该语法是 JavaScript 和 Rust 的核心特性之一。
      *
-     * ### 解引用裸指针
+     * ### 并发编程模型
+     * - OS 线程, 最简单的一种并发模型，无需改变任何编程业务/代码逻辑，非常适合作为语言的原生并发模型。当然，这种模型也有缺点，例如线程间的同步将变得更加困难，线程间的上下文切换损耗较大。使用线程池在一定程度上可以提升性能，但是对于 IO 密集的场景来说，线程池还是不够。
+     * - 协程(Coroutines) 可能是目前最火的并发模型，协程跟线程类似，无需改变编程模型，同时它也跟 async 类似，可以支持大量的任务并发运行。但协程抽象层次过高，导致用户无法接触到底层的细节，这对于系统编程语言和自定义异步运行时是难以接受的
+     * - actor 模型是 erlang 的杀手锏之一，它将所有并发计算分割成一个一个单元，这些单元被称为 actor , 单元之间通过消息传递的方式进行通信和数据传递，跟分布式系统的设计理念非常相像。由于 actor 模型跟现实很贴近，因此它相对来说更容易实现，但是一旦遇到流控制、失败重试等场景时，就会变得不太好用
+     * - 事件驱动(Event driven), 事件驱动模型性能相当好，常常和回调( Callback )一起使用。但事件驱动模型最大的问题就是存在回调地狱的风险：非线性的控制流和结果处理导致了数据流向和错误传播变得难以掌控，并且代码可维护性和可读性也大幅降低，大名鼎鼎的 JavaScript 曾经就存在回调地狱。
+     * - async/await， 该模型性能高，还能支持底层编程，同时又像线程和协程那样无需过多的改变编程模型，但有得必有失，async 模型的问题就是内部实现机制过于复杂，对于用户来说，理解和使用起来也没有线程和协程简单。
      *
-     * 裸指针(raw pointer，又称原生指针) 在功能上跟引用类似，它需要显式地注明可变性。
-     * 但是裸指针又和引用有所不同，裸指针有两种形式: `*const T` 和 `*mut T`，代表不可变和可变。
-     * `*` 操作符常见的含义是用于解引用，但是在裸指针 `*const T` 和 `*mut` 中，`*` 只是类型名称的一部分，并没有解引用的含义。
-     *
-     * 截至目前，已经有三种类似指针的概念：**引用、智能指针和裸指针**。裸指针与引用、智能指针不同：
-     * - 可以绕过 Rust 的借用规则，可以同时拥有一个数据的可变、不可变指针，甚至还能拥有多个可变的指针
-     * - 不能保证指向的内存是合法的
-     * - 可以是 null
-     * - 没有实现任何自动的回收 (drop)
-     *
-     * 使用裸指针可以创建两个可变指针都指向同一个数据，如果使用安全的 Rust 是无法做到这一点的，因为违背了借用规则。
-     * 因此虽然裸指针可以绕过借用规则，但是由此带来的数据竞争问题，需要程序员着重处理。
-     *
-     * 总之，裸指针跟 C 指针是非常像的，使用它需要以牺牲安全性为前提，但获得了更好的性能，也可以跟其它语言或硬件打交道。
-     *
-     * #### 基于引用创建裸指针
-     * 需要注意：**基于引用创建裸指针是安全的行为，而解引用裸指针才是不安全的行为**。即基于引用创建裸指针时不需要 unsafe，解引用时才需要。
-     *
-     * ```rust
-     * // 基于引用创建裸指针是安全的行为，解引用裸指针才是不安全的
-     * let mut num = 3;
-     * let num_ptr = &num as *const i32; // 创建裸指针是安全的
-     * let num_mutptr1 = &mut num as *mut i32; // 创建可变的裸指针，与不可变裸指针存储是一样的地址，但语义上是区分的
-     * let num_mutptr2 = &mut num as *mut i32; // 裸指针是可以创建多个可变的
-     * unsafe {
-     *     // *num_ptr = 4;
-     *     *num_mutptr1 = 4;
-     *     *num_mutptr2 = 4;
-     * }
-     * println!(
-     *     "num = {}, num_ptr = {:p}, num_mutptr1 = {:p}, num_mutptr2 = {:p}",
-     *     num, num_ptr, num_mutptr1, num_mutptr2
-     * );
-     * ```
-     *
-     * #### 基于智能指针创建裸指针
-     * 与基于引用创建裸指针很类似，基于智能指针创建裸指针是安全的，解引用才是不安全的行为。
-     * ```rust
-     * // 基于智能指针创建裸指针
-     * let mut num_box = Box::new(2);
-     * let num_box_ptr = &*num_box as *const i32;
-     * let num_box_mutptr = &mut *num_box as *mut i32;
-     * unsafe {
-     *     // *num_box_ptr = 4;
-     *     *num_box_mutptr = 4;
-     * }
-     * println!(
-     *     "num_box = {}, num_box_ptr = {:p}, num_box_mutptr = {:p}",
-     *     num_box, num_box_ptr, num_box_mutptr
-     * )
-     * ```
-     *
-     * #### 基于内存地址创建裸指针
-     * 基于一个内存地址来创建裸指针，可以想像这种行为是相当危险的。试图使用任意的内存地址往往是一种未定义的行为(undefined behavior)，因为该内存地址有可能存在值，也有可能没有。
-     * 同时编译器也有可能会优化这段代码，会造成没有任何内存访问发生，甚至程序还可能发生段错误(segmentation fault)。
-     *
-     * 正常项目几乎不会基于内存地址创建裸指针的做法。
+     * Rust 经过权衡取舍后，最终选择了同时提供多线程编程和 async 编程:
+     * - 多线程编程通过标准库实现，当无需高并发，例如需要并行计算时，就可以选择多线程，优点是线程内的代码执行效率更高、实现更直观更简单
+     * - async/await 通过语言特性 + 标准库 + 三方库的方式实现，在需要高并发、异步 I/O 时，就可以选择这种模型
      *
      *
-     * ### 调用 unsafe 函数或方法
-     * unsafe 函数从外表上来看跟普通函数并无区别，唯一的区别就是它需要使用 unsafe fn 来进行定义。
-     * 这种定义方式是为了告诉调用者：当调用此函数时需要注意它的相关需求，因为 Rust 无法担保调用者在使用该函数时能满足它所需的一切需求。
+     * ### async vs 多线程
+     * 虽然 async 和多线程都可以实现并发编程，但是这两种并发模型适用的场景不一样，并且方式并不互通，从一个方式切换成另一个需要大量的代码重构工作，因此提前为项目选择适合的并发模型就变得至关重要。
      *
-     * 在编写 unsafe 函数时，有一点需要注意：
-     * unsafe 函数体中无需使用 unsafe 语句块，unsafe 函数自身就是一个 unsafe 语句块，但一个函数包含了 unsafe 代码不代表需要将整个函数都定义为 unsafe fn。
+     * **OS 线程非常适合少量任务的并发或 CPU 密集型任务**
      *
-     * ```rust
-     * unsafe fn gen_unsafe() {
-     *     // 基于智能指针创建裸指针
-     *     let mut num_box = Box::new(2);
-     *     let num_box_ptr = &*num_box as *const i32;
-     *     let num_box_mutptr = &mut *num_box as *mut i32;
-     *     // *num_box_ptr = 4;
-     *     *num_box_mutptr = 4; // unsafe函数中无需unsafe语句块
+     * 因为线程的创建和上下文切换是非常昂贵的，甚至于空闲的线程都会消耗系统资源。虽说线程池可以有效的降低性能损耗，但是也无法彻底解决问题。
+     * 当然，线程模型也有其优点，例如它不会破坏代码逻辑和编程模型，原有的同步代码经过少量修改适配后就可以在新线程中直接运行。
+     * 同时在某些操作系统中，还可以改变线程的优先级，这对于实现驱动程序或延迟敏感的应用(例如硬实时系统)很有帮助。
      *
-     *     println!(
-     *         "gen_unsafe: num_box = {}, num_box_ptr = {:p}, num_box_mutptr = {:p}",
-     *         num_box, num_box_ptr, num_box_mutptr
-     *     );
-     * }
-     * unsafe { gen_unsafe() }
-     * ```
+     * 对于长时间运行的 CPU 密集型任务，例如并行计算，使用线程将更有优势。这种密集任务往往会让所在的线程持续运行，任何不必要的线程切换都会带来性能损耗，因此高并发反而在此时成为了一种多余。
+     * 需要注意，创建的线程数应该等于 CPU 核心数，以充分利用 CPU 的并行能力，甚至还可以将线程绑定到 CPU 核心上，进一步减少线程上下文切换。
      *
-     * ### 安全抽象包裹 unsafe 代码
+     * **async 更适合 IO 密集型任务（高并发任务）**
      *
-     * 一个函数包含了 unsafe 代码不代表需要将整个函数都定义为 unsafe fn。事实上，在标准库中有大量的安全函数，它们内部都包含了 unsafe 代码块，例如 split_at_mut。
+     * web 服务器、数据库连接等等网络服务，因为这些任务绝大部分时间都处于等待状态，如果使用多线程，那线程大量时间会处于空闲状态，再加上线程上下文切换的高昂代价，让多线程做 IO 密集任务变成了一件成本非常高的事。
+     * 而使用 async，既可以有效的降低 CPU 和内存的负担，又可以让大量的任务并发的运行，一个任务一旦处于 IO 或者其他等待(阻塞)状态，就会被立刻切走并执行另一个任务，而这里的任务切换的性能开销要远远低于使用多线程时的线程上下文切换。
      *
-     * 需求：将一个数组分成两个切片，且每一个切片都要求是可变的。类似这种需求在安全 Rust 中是很难实现的，因为要对同一个数组做两个可变借用，这不符合借用规则。
-     * ```rust
-     * fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
-     *     let len = slice.len();
-     *     assert!(mid <= len);
+     * 事实上, async 底层也是基于线程实现，但是它基于线程封装了一个运行时，可以将多个任务映射到少量线程上，然后将线程切换变成了任务切换，任务切换仅仅是内存中的访问，因此要高效的多。
+     * 当然 async 也有其缺点，编译器会为 async 函数生成状态机，然后将整个运行时打包进来，这会造成编译出的二进制可执行文件体积显著增大。
      *
-     *     (&mut slice[..mid], &mut slice[mid..]) // 出现多个可变借用，不符合 rust 的借用规则，编译失败
-     * }
-     * ```
+     * 总之，async 编程并没有比多线程更好，最终还是根据使用场景作出合适的选择。如果无需高并发，或者也不在意线程切换带来的性能损耗，那么多线程使用起来会简单、方便的多！
      *
-     * 使用 unsafe 绕过借用规则
-     * ```rust
-     * // 安全抽象包裹 unsafe 代码，即将一个unsafe语句块放在安全的rust中
-     * fn split_at_mut(_slice: &mut [i32], point: usize) -> (&mut [i32], &mut [i32]) {
-     *     let len = _slice.len();
-     *     assert!(point < len);
+     * - 有大量 IO 任务需要并发运行时，选 async 模型
+     * - 有部分 IO 任务需要并发运行时，选多线程模型，如果想要降低线程创建和销毁的开销，可以使用线程池
+     * - 有大量 CPU 密集任务需要并行运行时，例如并行计算，选多线程模型，且让线程数等于或者稍大于 CPU 核心数
+     * - 根据性能测试做出选择，或者统一选多线程，因为 async 模型可能要处理额外的问题
      *
-     *     let ptr = _slice.as_mut_ptr();
-     *     // (&mut _slice[..point], &mut _slice[point..]) 出现多个可变借用，不符合 rust 的借用规则，编译失败
-     *     unsafe {
-     *         // 从可变裸指针获取可变引用
-     *         (
-     *             slice::from_raw_parts_mut(ptr, point),
-     *             slice::from_raw_parts_mut(ptr.add(point), len - point),
-     *         )
-     *     }
-     * }
-     * let mut arr = [1, 2, 3, 4];
-     * println!("{:?}", split_at_mut(&mut arr, 2));
-     * ```
+     * > 如果使用 tokio，那 CPU 密集的任务尤其注意需要用多线程模型去处理，例如使用 spawn_blocking 创建一个阻塞的线程去完成相应 CPU 密集任务。
+     * > 至于具体的原因，不仅是多线程适合 CPU 密集型任务，还有一个是：tokio 是协作式的调度器，如果某个 CPU 密集的异步任务是通过 tokio 创建的，那理论上来说，该 CPU 密集型异步任务需要跟其它的异步任务交错执行，最终都得到了执行，皆大欢喜。
+     * > 但实际情况是，CPU 密集的任务很可能会一直霸占着 CPU，此时 tokio 的调度方式决定了该任务会一直被执行，这意味着，其它的异步任务无法得到执行的机会，最终这些任务都会因为得不到资源而**饿死**。
+     * > 而使用 spawn_blocking 后，会创建一个单独的 OS 线程，该线程并不会被 tokio 所调度(被 OS 所调度)，因此它所执行的 CPU 密集任务也不会导致 tokio 调度的那些异步任务被饿死。
      *
-     * 有几点需要注意：
-     * as_mut_ptr 会返回指向 slice 首地址的裸指针 `*mut i32`
-     * slice::from_raw_parts_mut 方法通过指针和长度来创建一个新的切片，是一个unsafe方法。简单来说，该切片的初始地址是 ptr，长度为 point
-     * ptr.add(point) 可以获取第二个切片的初始地址，是一个unsafe方法。由于切片中的元素是 i32 类型，每个元素都占用了 4 个字节的内存大小，因此不能简单的用 `ptr + mid` 来作为初始地址，而应该使用 `ptr + 4 * mid`，但是这种使用方式并不安全，因此 .add 方法是最佳选择
+     * **性能对比**
+     * |操作 |	async |	线程|
+     * | ---- | ---- | ---- |
+     * |创建 |	0.3 微秒 | 17 微秒|
+     * |线程切换 |	0.2 微秒 | 1.7 微秒|
      *
-     * ```rust
-     *  // 安全抽象包裹 unsafe 代码，即将一个unsafe语句块放在安全的rust中
-     *  fn split_at_mut(_slice: &mut [i32], point: usize) -> (&mut [i32], &mut [i32]) {
-     *      let len = _slice.len();
-     *      assert!(point < len);
+     * 可以看出，async 在线程切换的开销显著低于多线程，对于 IO 密集的场景，这种性能开销累计下来会非常可怕！
      *
-     *      let ptr = _slice.as_mut_ptr();
-     *      // (&mut _slice[..point], &mut _slice[point..]) 出现多个可变借用，不符合 rust 的借用规则，编译失败
-     *      unsafe {
-     *          // 从可变裸指针获取可变引用
-     *          (
-     *              slice::from_raw_parts_mut(ptr, point), // from_raw_parts_mut 通过指针和长度来创建一个新的切片，是一个unsafe方法
-     *              slice::from_raw_parts_mut(ptr.add(point), len - point), // ptr.add(point) 可以获取第二个切片的初始地址，是一个unsafe方法
-     *          )
-     *      }
-     *  }
-     *  let mut arr = [1, 2, 3, 4];
-     *  println!("{:?}", split_at_mut(&mut arr, 2));
-     * ```
+     * 在一些批量下载场景中就可以选择 async 模型，因为涉及到的 IO 密集场景，多线程模型的性能开销更高。
      *
-     * ### FFI 外部函数接口
-     * FFI（Foreign Function Interface）外部函数接口是用来与其它语言进行交互的接口设计，但并不是所有语言都称为 FFI。例如在 Java 中称之为 JNI（Java Native Interface）。
-     *
-     * FFI 之所以存在是现实中很多代码库都是由不同语言编写的，如果需要使用某个库，但它是由其它语言编写的，往往只有几个选择：
-     * - 对该库进行重写或者移植
-     * - 独立的服务调用（HTTP，gRPC）
-     * - 使用 FFI
-     *
-     * 在大部分情况下，重写或移植程序需要花费大量的时间和精力，独立的服务调用可能不满足时延，此时 FFI 就是最佳选择。
-     * 并且，在将其他语言的代码重构为 Rust 时，先将相关代码引入到 Rust 项目中，然后逐步重构，是一个非常不错的渐进式过程。
-     *
-     * 涉及到不同语言的交互，无法确定这个行为是否安全，因此 rust 的 FFI 需要 unsafe 的支持才能绕过编译器的审查，达到正常编译的目的。
-     *
-     * ```rust
-     * extern "C" {
-     *     fn abs(input: i32) -> i32;
-     * }
-     *
-     * unsafe {
-     *     println!("Absolute value of -3 according to C: {}", abs(-3));
-     * }
-     * ```
-     * C 语言的代码定义在了 extern 代码块中， 而 extern 必须使用 unsafe 才能进行进行调用，原因在于其它语言的代码并不会强制执行 Rust 的规则，因此 Rust 无法对这些代码进行检查，最终还是要靠开发者自己来保证代码的正确性和程序的安全性。
-     *
-     * > 阅读：
-     * > - https://rustwiki.org/zh-CN/book/ch19-01-unsafe-rust.html#使用-extern-函数调用外部代码
-     *
-     * #### ABI
-     * **应用二进制接口 ABI (Application Binary Interface) 定义了如何在汇编层面来调用该函数**。
-     *
-     * 在 extern "C" 代码块列出想要调用的外部函数的签名。其中 "C" 定义了外部函数所使用的 ABI。在所有 ABI 中，C 语言的是最常见的。
+     * 当然 async 和多线程并不是二选一，在同一应用中，可以根据情况两者一起使用，同理，其他并发模型也可以自由搭配使用。
      *
      *
-     * #### 其它语言调用 Rust 函数
-     * FFI 支持 rust 调用其他语言，也支持其他语言调用 rust。方法是使用 extern 来创建一个接口，其它语言可以通过该接口来调用相关的 Rust 函数。
+     * ### async/await
      *
-     * 供其他语言调用的 FFI 语法与调用其他语言的 FFI 有所不同，调用其他语言使用 extern 语句块，供其他语言调用是在函数定义时加上 extern 关键字。
-     * 除了加上 extern 关键字外，还需要加上 `#[no_mangle]` 注解，它的作用是告诉 Rust 编译器不要乱改函数的名称。
+     * 目前已经有诸多语言通过 async 的方式提供了异步编程，例如 JavaScript ，但 Rust 在实现上有所区别:
      *
-     * > Mangling：rust 编译时可能需要修改函数的名称，目的是为了让名称包含更多的信息，这样其它的编译部分就能从该名称获取相应的信息，这种修改会导致函数名变得相当不可读，并且使原函数名称失效。
+     * - Future 在 Rust 中是惰性的，只有在被轮询(poll)时才会运行，因此丢弃一个 future 会阻止它未来再被运行，即不再有可能被运行。可以将Future理解为一个在未来某个时间点被调度执行的任务
+     * - Async 在 Rust 中使用开销是零，意味着只有能看到的代码(自己编写的代码)才有性能损耗，你看不到的(async 内部实现)都没有性能损耗。简单理解，async 的状态机等相关是在编译器就已经生成，无需运行时再转换。可以无需分配任何堆内存、也无需任何动态分发来使用 async，这对于热点路径的性能有非常大的好处。正是得益于此，Rust 的异步编程性能才会这么高
+     * - Rust 没有内置异步调用所必需的运行时，但 Rust 社区生态中已经提供了非常优异的运行时实现，例如 tokio
+     * - 运行时同时支持单线程和多线程，这两者拥有各自的优缺点
      *
-     * ```rust
-     * #[no_mangle]
-     * pub extern "C" fn call_from_c() {
-     *     println!("Just called a Rust function from C!");
-     * }
-     * ```
+     * 目前 async 并发模型还没有达到多线程并发模型的成熟度，其中一部分内容还在不断进化中。当然，这并不影响在生产级项目中使用，社区中也有 tokio 这种成熟的库。
      *
-     * ### 访问或修改可变静态变量
-     * 在之前的全局变量章节中有介绍。
+     * #### 语言和库的支持
+     * async 的底层实现非常复杂，且会导致编译后文件体积显著增加，因此 Rust 没有选择像 Go 语言那样内置了完整的特性和运行时，而是选择了通过 Rust 语言提供了必要的特性支持，再通过社区来提供 async 运行时的支持。
      *
-     * ### unsafe 特征
-     * 之所以会有 unsafe 的特征，是因为该特征至少有一个方法包含有编译器无法验证的内容。unsafe 的特征并不常见，已接触的只有 Send。
-     * unsafe 特征需要使用 unsafe impl 实现方法，unsafe impl 通知编译器，程序相应的正确性由程序员保证。
+     * 所以，如果要完整的使用 async 异步编程，需要依赖以下语言特性特性、标准库以及外部库:
+     * - 最基础的特征(例如 Future)、类型和函数，由标准库提供实现
+     * - 关键字 async/await 由 Rust 语言提供，并进行了编译器层面的支持
+     * - 众多实用的类型、宏和函数由官方开发的 futures 包提供(不是标准库)，它们可以用于任何 async 应用中。
+     * - async 代码的执行、IO 操作、任务创建和调度等等复杂功能由社区的 async 运行时提供，例如 tokio 和 async-std
      *
-     * 阅读：https://course.rs/advance/unsafe/superpowers.html#实现-unsafe-特征
+     * 同时，在同步( synchronous )代码中使用的一些语言特性可能无法在 async 中使用，Rust 也不允许你在特征中声明 async 函数(可以通过三方库实现)。
      *
-     * ### 访问 union 中的字段
-     * union 主要用于跟 C 代码进行交互，访问 union 的字段是不安全的，因为 Rust 无法保证当前存储在 union 实例中的数据类型。
+     * #### 编译和错误
+     * 在大多数情况下，async 中的编译错误和运行时错误与普通的编译错误和运行时错误没有区别，但是依然有以下几点值得注意：
+     * - 编译错误，由于 async 编程时需要经常使用复杂的语言特性，例如生命周期和Pin，因此相关的错误可能会出现的更加频繁
+     * - 运行时错误，编译器会为每一个async函数生成状态机，这会导致在栈跟踪时会包含这些状态机的细节，同时还包含了运行时对函数的调用，因此，栈跟踪记录(例如 panic 时)将变得更加难以解读
+     * - 一些隐蔽的错误也可能发生，例如在一个 async 上下文中去调用一个阻塞的函数，或者没有正确的实现 Future 特征都有可能导致这种错误，这种错误可能会悄无声息的通过编译检查甚至有时候会通过单元测试
      *
-     * ```rust
-     * #[repr(C)]
-     * union MyUnion {
-     *     f1: u32,
-     *     f2: f32,
-     * }
-     * ```
-     * union 的使用方式与结构体很相似，但是 union 的所有字段都共享同一个存储空间，意味着往 union 的某个字段写入值，会导致其它字段的值会被覆盖。
+     * #### 兼容性考虑
+     * 与 JavaScript 不一样，**rust 的异步代码和同步代码并不总能和睦共处**。例如，无法在一个同步函数中去调用一个 async 异步函数，同步和异步代码也往往使用不同的设计模式，这些都会导致两者融合上的困难。
      *
-     * ### 实用工具库
-     * unsafe 和 FFI 在 Rust 的使用场景中是相当常见，因此社区已经开发出一些实用的工具，可以改善相应的开发体验。这一部分可以在开发中尝试不同的工具。
+     * 甚至异步代码之间也会存在类似的问题，例如如果一个库依赖于特定的 async 运行时来运行，那么这个库非常有必要告诉它的用户，它用了某个运行时。否则一旦用户选了不同的或不兼容的运行时，就会导致不可预知的麻烦。
      *
-     * #### rust-bindgen 和 cbindgen
-     * 对于 FFI 调用来说，保证接口的正确性是非常重要的，这两个库可以帮我们自动生成相应的接口。
-     * 其中 rust-bindgen 用于生成在 Rust 中访问 C 的代码，而 cbindgen 则相反，用于生成在 C 中访问 Rust 的代码。
+     * #### 性能特性
+     * 目前主流的 async 运行时都使用了多线程实现，相比单线程虽然增加了并发表现，但是对于执行性能会有所损失，因为多线程实现会有同步和切换上的性能开销。
      *
-     * #### cxx
-     * 如果需要跟 C++ 代码交互，则推荐使用 cxx，它提供了双向的调用，最大的优点就是安全，无需使用 unsafe 语句块。
+     * 如果需要**极致的顺序执行性能**，那么 async 目前并不是一个好的选择。
+     * 同样的，对于**延迟敏感的任务**来说，任务的执行次序需要能被严格掌控，而不是交由运行时去自动调度，后者会导致不可预知的延迟。
+     * 例如一个 web 服务器总是有 1% 的请求，它们的延迟会远高于其它请求，因为调度过于繁忙导致了部分任务被延迟调度，最终导致了较高的延时。
+     * 正因为此，这些延迟敏感的任务非常依赖于运行时或操作系统提供调度次序上的支持。
      *
-     * #### Miri
-     * miri 可以生成 Rust 的中间层表示 MIR，它可以帮助检查常见的未定义行为(UB = Undefined Behavior)，例如
-     * - 内存越界检查和内存释放后再使用(use-after-free)
-     * - 使用未初始化的数据
-     * - 数据竞争
-     * - 内存对齐问题
-     *
-     * 可以通过 rustup component add miri 来安装它，并通过 cargo miri 来使用，同时还可以使用 cargo miri test 来运行测试代码。
-     * 但需要注意的是，它只能帮助识别被执行代码路径的风险，那些未被执行到的代码是没办法被识别的。
-     *
-     * #### Prusti
-     * prusti 需要自己来构建一个证明，然后通过它证明代码中的不变量是正确被使用的，当在安全代码中使用不安全的不变量时，就会非常有用。
-     * 阅读：https://viperproject.github.io/prusti-dev/user-guide/
-     *
-     * #### Clippy
-     * 官方的 clippy 检查器提供了有限的 unsafe 支持，虽然不多但是至少有一定帮助。例如 missing_safety_docs 检查可以帮助检查哪些 unsafe 函数遗漏了文档。
-     * 需要注意的是：Rust 编译器并不会默认开启所有检查，可以调用 rustc -W help 来看看最新的信息。
-     *
-     * #### 模糊测试(fuzz testing)
-     * 在 Rust Fuzz Book 中列出了一些 Rust 可以使用的模糊测试方法。同时还可以使用 rutenspitz 这个过程宏来测试有状态的代码，例如数据结构。
-     *
-     * ### 总结
-     * unsafe 只应该用于仅限的五种场景，其它场景应该坚决的使用安全的代码。
-     * 总之，能不使用 unsafe 一定不要使用，就算使用也要控制好边界，让范围尽可能的小，只有真的需要 unsafe 的代码才应该包含其中, 而不是将无关代码也纳入进来。
+     * 以上的两个需求，目前的 async 运行时并不能很好的支持，在未来可能会有更好的支持，但在此之前，可以尝试用多线程解决。
      * 
-     * ### 进一步学习
-     * - https://blog.logrocket.com/unsafe-rust-how-and-when-not-to-use-it/
      *
      */
-
-    // 基于引用创建裸指针是安全的行为，解引用裸指针才是不安全的
-    let mut num = 3;
-    let num_ptr = &num as *const i32; // 创建裸指针是安全的
-    let num_mutptr1 = &mut num as *mut i32; // 创建可变的裸指针，与不可变裸指针存储是一样的地址，但语义上是区分的
-    let num_mutptr2 = &mut num as *mut i32; // 裸指针是可以创建多个可变的
-    unsafe {
-        // *num_ptr = 4;
-        *num_mutptr1 = 4;
-        *num_mutptr2 = 4;
-    }
-    println!(
-        "num = {}, num_ptr = {:p}, num_mutptr1 = {:p}, num_mutptr2 = {:p}",
-        num, num_ptr, num_mutptr1, num_mutptr2
-    );
-
-    // 基于智能指针创建裸指针
-    let mut num_box = Box::new(2);
-    let num_box_ptr = &*num_box as *const i32;
-    let num_box_mutptr = &mut *num_box as *mut i32;
-    unsafe {
-        // *num_box_ptr = 4;
-        *num_box_mutptr = 4;
-    }
-    println!(
-        "num_box = {}, num_box_ptr = {:p}, num_box_mutptr = {:p}",
-        num_box, num_box_ptr, num_box_mutptr
-    );
-
-    unsafe fn gen_unsafe() {
-        // 基于智能指针创建裸指针
-        let mut num_box = Box::new(2);
-        let num_box_ptr = &*num_box as *const i32;
-        let num_box_mutptr = &mut *num_box as *mut i32;
-        // *num_box_ptr = 4;
-        *num_box_mutptr = 4;
-
-        println!(
-            "gen_unsafe: num_box = {}, num_box_ptr = {:p}, num_box_mutptr = {:p}",
-            num_box, num_box_ptr, num_box_mutptr
-        );
-    }
-    unsafe { gen_unsafe() }
-
-    // 安全抽象包裹 unsafe 代码，即将一个unsafe语句块放在安全的rust中
-    fn split_at_mut(_slice: &mut [i32], point: usize) -> (&mut [i32], &mut [i32]) {
-        let len = _slice.len();
-        assert!(point < len);
-
-        let ptr = _slice.as_mut_ptr();
-        // (&mut _slice[..point], &mut _slice[point..]) 出现多个可变借用，不符合 rust 的借用规则，编译失败
-        unsafe {
-            // 从可变裸指针获取可变引用
-            (
-                slice::from_raw_parts_mut(ptr, point), // from_raw_parts_mut 通过指针和长度来创建一个新的切片，是一个unsafe方法
-                slice::from_raw_parts_mut(ptr.add(point), len - point), // ptr.add(point) 可以获取第二个切片的初始地址，是一个unsafe方法
-            )
-        }
-    }
-    let mut arr = [1, 2, 3, 4];
-    println!("{:?}", split_at_mut(&mut arr, 2));
-
-    // FFI
-    extern "C" {
-        fn abs(input: i32) -> i32;
-    }
-    unsafe { println!("{}", abs(-3)) }
 }
