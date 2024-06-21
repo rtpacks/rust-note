0d86eb9caf6bba69fc6f574658e93d40905a61b5
Mr.MF <Mr.MF@foxmail.com>
2024-06-22 00:02:50 +0800

    feat: unit 70-Macro 宏编程

---
+++ src/main 70-Macro 宏编程.rs
@@ -0,0 +1,18 @@
+use core::fmt;
+use std::{io, num, slice};
+
+fn main() {
+    /*
+     *
+     * ## Macro 宏编程
+     *
+     * 宏在 Rust 中无处不在。在 Rust 中宏分为两大类：声明式宏( declarative macros ) macro_rules! 和三种过程宏( procedural macros )。
+     * 
+     * ### 宏和函数的区别
+     * https://course.rs/advance/macro.html#%E5%AE%8F%E5%92%8C%E5%87%BD%E6%95%B0%E7%9A%84%E5%8C%BA%E5%88%AB
+     *
+     * ### others
+     * 暂时不学习，等有需求时再阅读：https://course.rs/advance/macro.html#macro-%E5%AE%8F%E7%BC%96%E7%A8%8B
+     *
+     */
+}
