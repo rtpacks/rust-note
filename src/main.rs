e6f834dc94ffdbfbf2efcd11756be0f2d58627ea
Mr.MF <Mr.MF@foxmail.com>
2024-07-06 00:49:23 +0800

    feat: unit 73-async 异步编程：Pin 和 Unpin

--- src/main.rs
+++ src/main.rs
@@ -94,16 +94,20 @@ fn main() {
      * - 如果 Pin 的是 !Unpin 类型，则无法被移动走，因为 !Unpin 就表示移动是不安全的，Pin 在 Safe Rust 中没有提供获取可变引用的方法
      *
      * 记住 Pin 规则：
-     * - Pin 代理一个值的引用，如 `Pin<Box<T>>` 和 `Pin<T>` 就是不同的代理引用。通过 Pin 去操作原始数据的行为都会收到 Pin 的管制，在使用上非常类似 newtype，仅提供部分或不提供代理操作。直接操作原始数据就是不走 Pin 的管制。
-     * - Pin 固定 `!Unpin` 特征的类型关键就在禁止其被修改移动，被固定也就无法被修改移动，也就是无法在 Safe Rust 的情况下拿到 `Pin<!Unpin>` 类型的可变引用，确保内存安全。
-     * - Pin 做的事情就是制定一个编译器规则，在使用自引用类型的时候给予帮助/提示，并禁止在 Safe Rust 中编写异常的代码，如禁止移动实现了 `!Unpin` 特征的数据类型。如果非要在 unsafe 中做可能发生异常的动作，那么 Pin 将没有任何作用。
+     * - Pin 代理一个值的引用，如 `Pin<Box<T>>` 和 `Pin<T>` 就是不同的代理引用。通过 Pin 去操作原始数据的行为都会收到 Pin 的管制，在使用上非常类似 newtype，仅提供部分或不提供代理操作。如果直接操作原始数据，那么 Pin 的管制不会生效
+     * - Pin 固定 `!Unpin` 特征的类型关键就在禁止其被修改移动，被固定也就无法被修改移动，也就是无法在 Safe Rust 的情况下拿到 `Pin<impl !Unpin>` 类型的可变引用，确保内存安全
+     * - Pin 只限制 `!Unpin` 不可移动，如果一个结构体类型实现了 `!Unpin`，同时具有 `Unpin` 属性，那么 `Unpin` 属性是可以安全移动的，即该属性的值在 Safe Rust 中是可以修改的
+     * - Pin 做的事情就是制定一个编译器规则，在使用自引用类型的时候给予帮助/提示，并禁止在 Safe Rust 中编写异常的代码，如禁止移动实现了 `!Unpin` 特征的数据类型。如果非要在 unsafe 中做可能发生异常的动作，那么 Pin 将没有任何作用
      *
-     * > 注意，Pin 管理的是引用，而不是真实数据。通过使用 Pin，让 Pin 在 Safe Rust 中帮助校验程序是否会有异常，这就是使用 Pin 的本质。如果直接操作原始数据，就是不走 Pin 的管制。
+     * > 注意，Pin 管理的是引用，而不是真实数据。通过使用 Pin，让 Pin 在 Safe Rust 中帮助校验程序是否会有异常，这就是使用 Pin 的本质。如果直接操作原始数据，Pin 的管制不会生效。
      *
+     * ### Pin 的使用
      * 什么时候使用 Pin？如果不希望某个被引用的内容发生改变，就可以使用 Pin。
      * 如 Test 结构体是一个自引用结构体，如果移动会发生意外的副作用，所以它的移动应该是被禁止的，使用 Pin 固定地址，禁止其移动。
      *
      * Pin 只会固定 `!Unpin` 类型，`Unpin` 类型不受影响。
+     *
+     * #### Pin 与 Unpin
      * Pin 固定实现 `Unpin` 特征的类型时，因为 Unpin 是移动安全的，所以在 **Safe Rust** 的环境下，`Pin<&impl Unpin>` 提供两种安全的方式获取值的可变引用：
      * - 通过 `Pin::get_mut()` 安全的获取可变引用
      * - 实现 Unpin 的类型，再实现 `DerefMut` 特征，就可以直接解引用拿到可变引用。其实这个也是通过 `get_mut` 获取的。
@@ -126,34 +130,164 @@ fn main() {
      *     let a_pin = Pin::new(&mut a);
      *     let a_mutref = a_pin.get_mut();
      *     println!("{}", a_mutref.a);
+     *     a_mutref.a = 12; // Pin 固定实现了 `Unpin` 特征的类型的数据是不限制的，可以修改数据
+     *     println!("{}", a_mutref.a);
      * }
      * ```
      *
      * > 注意，Pin 管理的是引用，而不是真实数据。通过使用 Pin，让 Pin 在 Safe Rust 中帮助校验程序是否会有异常，这就是使用 Pin 的本质。如果直接操作原始数据，就是不走 Pin 的管制。
      *
+     * Pin 固定引用有两个方式：**将引用固定在栈上（引用存储在栈上）**或**将引用固定在堆上（引用存储在堆上）**，注意不是值，是值得引用。
+     * 一定记住 Pin 的本质是通过 Pin 代理原始数据的引用，通过 Pin 去操作原始数据才会收到 Pin 的管制。
      *
-     * Pin 固定引用有两个方式：将引用固定在栈上（存储在栈上）或将引用固定在堆上（存储在堆上）。
+     * Pin 将引用固定在栈上：
+     * ```rust
+     * let a = A { a: 12 }; // 不可变引用
+     * let a_pin = Pin::new(&a);
+     * let a_ref = a_pin.get_ref();
      *
-     * 以上的方式是直接将值固定在栈上，
      *
-     * // TODO
+     * let mut a = A { a: 12 }; // 可变引用
      *
+     * // 直接操作原始数据，不经过 Pin 的管制
+     * {
+     *     a.a = 3;
+     * }
+     *
+     * // 通过 Pin 操作原始数据，收到 Pin 的管制
+     * {
+     *     let a_pin = Pin::new(&mut a);
+     *     let a_mutref = a_pin.get_mut();
+     *     println!("{}", a_mutref.a);
+     *     a_mutref.a = 12; // Pin 固定实现了 `Unpin` 特征的类型的数据是不限制的，可以修改数据
+     *     println!("{}", a_mutref.a);
+     * }
+     * ```
+     * 不可变引用只能使用 get_ref，Pin 没有为不可变引用提供 get_mut 函数，因为 Safe Rust 不能通过不可变引用修改数据。
+     *
+     * Pin 将引用固定在堆上：
+     * ```rust
+     * // 将引用固定在堆上
+     * let a = A { a: 112 };
+     * // let mut a_box = Pin::new(Box::new(&a)); 等于 Box::pin
+     *
+     * let mut a_box = Box::pin(&a);
+     * // a_box.as_mut().get_mut().a = 12; 错误的，不能通过不可变引用修改数据
+     *
+     * let mut a = A { a: 12 };
+     * // 直接操作原始数据，不经过 Pin 的限制
+     * {
+     *     a.a = 122;
+     * }
+     * {
+     *     let mut a_box = Box::pin(&mut a);
+     *     println!("{}", a_box.as_mut().get_mut().a);
+     *     a_box.as_mut().get_mut().a = 12;
+     *     println!("{}", a_box.as_mut().get_mut().a);
+     * }
+     * ```
+     *
+     *
+     * #### Pin 与 !Unpin
      * Pin 固定实现 `!Unpin` 特征的类型时，有两种方式：
      * - 使用 PhantomPinned，PhantomPinned 实现了 `!Unpin`，只要一个类型属性是 `!Unpin`，这个结构体就默认成为实现 `!Unpin` 的类型
      * - 手动实现 impl !Unpin for Test {}
+     *
+     * 前面提到过：Pin 只限制 `!Unpin` 不可移动，如果一个结构体类型实现了 `!Unpin`，同时具有 `Unpin` 属性，那么 `Unpin` 属性是可以安全移动的，即该属性的值在 Safe Rust 中是可以修改的。
+     *
+     * 在使用 Pin 与 `!Unpin` 时需要分清楚 Pin 管理的对象是谁，这样在编写代码时才不会出错。
      *
      * ```rust
      * #[derive(Debug)]
      * struct Test {
      *     a: String,
      *     b: *const String,
-     *     _pin: PhantomPinned, // 用 PhantomPinned 标识该类型是一个实现 `!Unpin` 的类型，只要一个类型属性是 `!Unpin`，这个结构体就默认成为实现 `!Unpin` 的类型
+     *     _pin: PhantomPinned,
+     * }
+     *
+     * impl Test {
+     *     fn new(txt: &str) -> Pin<Box<Self>> {
+     *         let t = Test {
+     *             a: String::from(txt),
+     *             b: std::ptr::null(),
+     *             _pin: PhantomPinned,
+     *         };
+     *         let mut boxed = Box::pin(t);
+     *         let self_ptr: *const String = &boxed.as_ref().a;
+     *         unsafe { boxed.as_mut().get_unchecked_mut().b = self_ptr };
+     *
+     *         boxed
+     *     }
+     *
+     *     fn new_mut(txt: &str) -> Pin<Box<&mut Self>> {
+     *         let t = Test {
+     *             a: String::from(txt),
+     *             b: std::ptr::null(),
+     *             _pin: PhantomPinned,
+     *         };
+     *         let t = Box::leak(Box::new(t));
+     *         let mut boxed = Box::pin(t);
+     *         let self_ptr: *const String = &boxed.as_ref().a;
+     *         unsafe { boxed.as_mut().get_unchecked_mut().b = self_ptr };
+     *
+     *         boxed
+     *     }
+     *
+     *     fn a<'a>(self: Pin<&'a Self>) -> &'a str {
+     *         &self.get_ref().a
+     *     }
+     *
+     *     fn b<'a>(self: Pin<&'a Self>) -> &'a String {
+     *         unsafe { &*(self.b) }
+     *     }
+     * }
+     *
+     * let mut test1 = Test::new("test1");
+     * let t = test1.as_mut();
+     * // t.a = String::from("Hello World");
+     *
+     * let mut test1 = Test::new_mut("test1");
+     * let t = test1.as_mut().get_mut();
+     * // a 的值发生变化是不影响安全的，因为 Pin 管理的引用指向的值，并没有改变地址。
+     * // 也就是说，Pin 固定的引用指向的值，只要这个值没有发生改变，就不会出现非移动安全问题
+     * // 这也很容易解释，t.a 实现的是 Unpin，是移动安全的，t.b 访问的是固定地址，t.a修改后，只是改地址指向的内容发生了变化，但是地址是不变的
+     * // t 才是 !Unpin，是非移动安全的，它发生改变，可能会导致 b 存储的指针指向错误的地址
+     * t.a = String::from("Hello World");
+     * unsafe {
+     *     println!("{}, {}", t.a, *t.b);
      * }
      * ```
      *
-     * TODO 使用 Pin 禁止自引用结构体在 Safe Rust 中移动
+     * 在 Pin 的管制下，`!Unpin` 在 Safe Rust 中是不允许移动的，即不允许修改当前值的地址。
+     * 但和上例一样，Pin 只限制 `!Unpin` 不可移动，如果一个结构体类型实现了 `!Unpin`，同时具有 `Unpin` 属性，那么 `Unpin` 属性是可以安全移动的，即该属性的值在 Safe Rust 中是可以修改的。
+     * 这也很容易解释，如果结构体某个属性 A 实现了 Unpin，那么它是移动安全的，另外一个属性 B 存储该地址，访问的是固定地址，A 地址的内容修改后，只是改地址指向的内容发生了变化，但是地址是不变的。
+     * 所以，Pin 对 `!Unpin` 才会有限制作用。
      *
-     * 如果将 Unpin 与之前的 Send/Sync 进行对比，会发现它们都很像：
+     * ```rust
+     * let mut test1 = Test::new("test1");
+     * let mut test2 = Test::new("test2");
+     * println!("a: {}, b: {}", test1.as_ref().a(), test1.as_ref().b());
+     * // 使用swap()函数交换两者，这里发生了 move(移动)，在 Pin 的管制下，!Unpin 在 Safe Rust 中是不允许移动的，即 Pin 管理的对象的地址不能发生变化
+     * // std::mem::swap(&mut *test1, &mut *test2);
+     * // std::mem::swap(test1.as_mut().get_mut(), test2.as_mut().get_mut());
+     * println!("a: {}, b: {}", test2.as_ref().a(), test2.as_ref().b());
+     *
+     *
+     * let mut test1 = Test::new_mut("test1");
+     * let t = test1.as_mut().get_mut();
+     * // a 的值发生变化是不影响安全的，因为 Pin 管理的引用指向的值，并没有改变地址。
+     * // 也就是说，Pin 固定的引用指向的值，只要这个值没有发生改变，就不会出现非移动安全问题
+     * // 这也很容易解释，t.a 实现的是 Unpin，是移动安全的，t.b 访问的是固定地址，t.a 修改后，只是改地址指向的内容发生了变化，但是地址是不变的
+     * // t 才是 !Unpin，是非移动安全的，它发生改变，可能会导致 b 存储的指针指向错误的地址
+     * t.a = String::from("Hello World");
+     * unsafe {
+     *     println!("{}, {}", t.a, *t.b);
+     * }
+     * ```
+     * 
+     * ### 标记特征
+     *
+     * 如果将 Unpin 和 !Unpin 与之前的 Send/Sync 进行对比，会发现它们都很像：
      * - 都是标记特征( marker trait )，该特征未定义任何行为，非常适用于标记
      * - 都可以通过 `!` 语法去除实现
      * - 绝大多数情况都是自动实现, 无需额外关注
@@ -181,6 +315,7 @@ fn main() {
      * ### 参考阅读
      * - https://folyd.com/blog/rust-pin-unpin/
      * - https://folyd.com/blog/rust-pin-advanced/#pin-shi-xian-de-trait
+     * - https://course.rs/advance/async/pin-unpin.html
      *
      * 归档
      * - https://web.archive.org/web/20240627082751/https://folyd.com/blog/rust-pin-unpin/
@@ -241,8 +376,6 @@ fn main() {
     struct A {
         a: i32,
     }
-    let mut a_ins = Box::pin(A { a: 1 });
-    a_ins.as_mut().get_mut().a = 2;
 
     let a = A { a: 12 }; // 不可变引用
     let a_pin = Pin::new(&a);
@@ -257,6 +390,25 @@ fn main() {
         let a_pin = Pin::new(&mut a);
         let a_mutref = a_pin.get_mut();
         println!("{}", a_mutref.a);
+        a_mutref.a = 12; // Pin 固定实现了 `Unpin` 特征的类型的数据是不限制的，可以修改数据
+        println!("{}", a_mutref.a);
+    }
+
+    // 将引用固定在堆上
+    let a = A { a: 112 };
+    // let mut a_box = Pin::new(Box::new(&a)); 等于 Box::pin
+    let mut a_box = Box::pin(&a);
+    // a_box.as_mut().get_mut().a = 12; 错误的，不能通过不可变引用修改数据
+    let mut a = A { a: 12 };
+    // 直接操作原始数据，不经过 Pin 的限制
+    {
+        a.a = 122;
+    }
+    {
+        let mut a_box = Box::pin(&mut a);
+        println!("{}", a_box.as_mut().get_mut().a);
+        a_box.as_mut().get_mut().a = 12;
+        println!("{}", a_box.as_mut().get_mut().a);
     }
 
     #[derive(Debug)]
@@ -280,6 +432,20 @@ fn main() {
             boxed
         }
 
+        fn new_mut(txt: &str) -> Pin<Box<&mut Self>> {
+            let t = Test {
+                a: String::from(txt),
+                b: std::ptr::null(),
+                _pin: PhantomPinned,
+            };
+            let t = Box::leak(Box::new(t));
+            let mut boxed = Box::pin(t);
+            let self_ptr: *const String = &boxed.as_ref().a;
+            unsafe { boxed.as_mut().get_unchecked_mut().b = self_ptr };
+
+            boxed
+        }
+
         fn a<'a>(self: Pin<&'a Self>) -> &'a str {
             &self.get_ref().a
         }
@@ -290,10 +456,24 @@ fn main() {
     }
 
     let mut test1 = Test::new("test1");
-    let mut test2 = Test::new("test2");
+    let t = test1.as_mut();
+    // t.a = String::from("Hello World");
 
+    let mut test1 = Test::new_mut("test1");
+    let t = test1.as_mut().get_mut();
+    // a 的值发生变化是不影响安全的，因为 Pin 管理的引用指向的值，并没有改变地址。
+    // 也就是说，Pin 固定的引用指向的值，只要这个值没有发生改变，就不会出现非移动安全问题
+    // 这也很容易解释，t.a 实现的是 Unpin，是移动安全的，t.b 访问的是固定地址，t.a修改后，只是改地址指向的内容发生了变化，但是地址是不变的
+    // t 才是 !Unpin，是非移动安全的
+    t.a = String::from("Hello World");
+    unsafe {
+        println!("{}, {}", t.a, *t.b);
+    }
+
+    let mut test1 = Test::new("test1");
+    let mut test2 = Test::new("test2");
     println!("a: {}, b: {}", test1.as_ref().a(), test1.as_ref().b());
-    // 使用swap()函数交换两者，这里发生了 move(移动)
+    // 使用swap()函数交换两者，这里发生了 move(移动)，在 Pin 的管制下，!Unpin 在 Safe Rust 中是不允许移动的，即 Pin 管理的对象的地址不能发生变化
     // std::mem::swap(&mut *test1, &mut *test2);
     // std::mem::swap(test1.as_mut().get_mut(), test2.as_mut().get_mut());
     println!("a: {}, b: {}", test2.as_ref().a(), test2.as_ref().b());
