4736cf06c0da84d530b901390160577f8f658e78
Mr.MF <Mr.MF@foxmail.com>
2024-07-29 13:36:33 +0800

    feat: 86-实战：mini-redis - client - IO & Frame

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use mini_redis::Result;
 use tokio::{
     fs::File,
     io::{self, AsyncReadExt, AsyncWriteExt},
+    net::{self, TcpListener},
 };
 
 #[tokio::main]
@@ -92,55 +93,148 @@ async fn main() -> Result<()> {
      *
      * io::copy(&mut buffer, &mut file).await.unwrap();
      * ```
-     * 
+     *
      * ### 回声服务 （Echo）
-     * 
+     * 如同写代码必写 hello, world，实现 web 服务器，往往会选择实现一个回声服务。该服务会将用户的输入内容直接返回给用户，就像回声壁一样。
+     * 具体来说，就是从用户建立的 TCP 连接的 socket 中读取到数据，然后立刻将同样的数据写回到该 socket 中。因此客户端会收到和自己发送的数据一模一样的回复。
      *
+     * 和 async Web 服务器实现类似，基本的服务器框架：通过 loop 循环接收 TCP 连接，然后为每一条连接创建一个单独的任务去处理。
      *
+     * ```rust
+     * let listener = TcpListener::bind("127.0.0.1:6330").await?;
      *
+     * async fn process(stream: net::TcpStream) {}
      *
+     * loop {
+     *     let (stream, addr) = listener.accept().await?;
+     *     tokio::spawn(async move { process(stream).await });
+     * }
+     * ```
+     *
+     * 然后使用 `io::copy` 函数完成回声服务。copy 函数有两个参数：读取器的可变引用，写入器的可变引用，现在需要将读取器中的数据直接拷贝到写入器中。
+     *
+     * 在当前服务中，读取器和写入器都是 stream，根据借用规则，copy 不能同时操作两个 stream 的可变引用：
+     * ```rust
+     * io::copy(&mut stream, &mut stream).await
+     * ```
+     *
+     * 借用规则限制只能操作一个变量的一个可变引用，这里 stream 不能既做读取器又做写入器。
+     * 任何一个读写器( reader + writer )都可以使用 io::split 方法进行分离，最终返回一个读取器和写入器，这两者可以单独使用。
+     * 实际上，io::split 可以用于任何同时实现了 AsyncRead 和 AsyncWrite 的值，它的内部使用了 Arc 和 Mutex 来实现相应的功能。
+     *
+     * ```rust
+     * let listener = TcpListener::bind("127.0.0.1:6330").await?;
+     *
+     * async fn process(mut stream: net::TcpStream) {
+     *     let (mut reader, mut writer) = io::split(stream);
+     *
+     *     if io::copy(&mut reader, &mut writer).await.is_err() {
+     *         eprintln!("failed to copy");
+     *     };
+     * }
+     *
+     * loop {
+     *     let (stream, addr) = listener.accept().await?;
+     *     tokio::spawn(async move { process(stream).await });
+     * }
+     * ```
+     *
+     * `io::split` 利用 Mutex 会有一定的性能损耗，还有两种方式可以分离读写器：
+     * - TcpStream::split会获取字节流的引用，然后将其分离成一个读取器和写入器。但由于使用了引用的方式，它们俩必须和 split 在同一个任务中。 优点就是，这种实现没有性能开销，因为无需 Arc 和 Mutex。
+     * - TcpStream::into_split还提供了一种分离实现，分离出来的结果可以在任务间移动，内部是通过 Arc 实现。
+     *
+     * ```rust
+     * let listener = TcpListener::bind("127.0.0.1:6330").await?;
+     *
+     * async fn process(mut stream: net::TcpStream) {
+     *     let (mut reader, mut writer) = stream.split();
+     *
+     *     if io::copy(&mut reader, &mut writer).await.is_err() {
+     *         eprintln!("failed to copy");
+     *     };
+     * }
+     *
+     * loop {
+     *     let (stream, addr) = listener.accept().await?;
+     *     tokio::spawn(async move { process(stream).await });
+     * }
+     * ```
      *
      *
      */
 
+    // {
+    //     // 部分数据结构如 `Vec<u8>、&[u8]` 也实现了它们：，支持直接将这些**数据结构作为读写器( reader / writer)**，一些常见的 buffer 容器其实就可以视为读取器和写入器。
+    //     let mut file = File::open(r"Cargo.toml").await.unwrap();
+    //     // 写入器
+    //     let mut buffer = [0; 1024];
+    //     // 由于 buffer 的长度限制，当次的 `read` 调用最多可以从文件中读取 1024 个字节的数据
+    //     let n = file.write(&mut buffer).await.unwrap();
+    //     println!("The bytes: {:?}", buffer);
+
+    //     let mut file = File::open("Cargo.toml").await.unwrap();
+    //     // 写入器
+    //     let mut buffer = Vec::new();
+    //     let n = file.read_to_end(&mut buffer).await.unwrap();
+    //     println!("The bytes: {:?}", buffer);
+    // }
+
+    // {
+    //     // 部分数据结构如 `Vec<u8>、&[u8]` 也实现了它们：，支持直接将这些**数据结构作为读写器( reader / writer)**，一些常见的 buffer 容器其实就可以视为读取器和写入器。
+    //     let mut file = File::create("public/foo.txt").await?;
+    //     // 读取器
+    //     // let buffer = "Hello World".as_bytes();
+    //     let buffer = b"Hello World";
+    //     let n = file.write(buffer).await.unwrap();
+    //     println!("Wrote the first {} bytes of 'some bytes'.", n);
+
+    //     let mut file = File::create(r"public/foo.txt").await?;
+    //     // 读取器
+    //     // let buffer = "Hello World".as_bytes();
+    //     let buffer = b"Hello World";
+    //     file.write_all(buffer).await.unwrap();
+    // }
+
+    // {
+    //     let mut file = File::create(r"public/foo.txt").await?;
+    //     // 读取器
+    //     let mut buffer = "Hello World".as_bytes();
+
+    //     io::copy(&mut buffer, &mut file).await.unwrap();
+    // }
+
+    // {
+    //     let listener = TcpListener::bind("127.0.0.1:6330").await?;
+
+    //     async fn process(mut stream: net::TcpStream) {
+    //         let (mut reader, mut writer) = io::split(stream);
+
+    //         if io::copy(&mut reader, &mut writer).await.is_err() {
+    //             eprintln!("failed to copy");
+    //         };
+    //     }
+
+    //     loop {
+    //         let (stream, addr) = listener.accept().await?;
+    //         tokio::spawn(async move { process(stream).await });
+    //     }
+    // }
+
     {
-        // 部分数据结构如 `Vec<u8>、&[u8]` 也实现了它们：，支持直接将这些**数据结构作为读写器( reader / writer)**，一些常见的 buffer 容器其实就可以视为读取器和写入器。
-        let mut file = File::open(r"Cargo.toml").await.unwrap();
-        // 写入器
-        let mut buffer = [0; 1024];
-        // 由于 buffer 的长度限制，当次的 `read` 调用最多可以从文件中读取 1024 个字节的数据
-        let n = file.write(&mut buffer).await.unwrap();
-        println!("The bytes: {:?}", buffer);
+        let listener = TcpListener::bind("127.0.0.1:6330").await?;
 
-        let mut file = File::open("Cargo.toml").await.unwrap();
-        // 写入器
-        let mut buffer = Vec::new();
-        let n = file.read_to_end(&mut buffer).await.unwrap();
-        println!("The bytes: {:?}", buffer);
-    }
+        async fn process(mut stream: net::TcpStream) {
+            let (mut reader, mut writer) = stream.split();
 
-    {
-        // 部分数据结构如 `Vec<u8>、&[u8]` 也实现了它们：，支持直接将这些**数据结构作为读写器( reader / writer)**，一些常见的 buffer 容器其实就可以视为读取器和写入器。
-        let mut file = File::create("public/foo.txt").await?;
-        // 读取器
-        // let buffer = "Hello World".as_bytes();
-        let buffer = b"Hello World";
-        let n = file.write(buffer).await.unwrap();
-        println!("Wrote the first {} bytes of 'some bytes'.", n);
+            if io::copy(&mut reader, &mut writer).await.is_err() {
+                eprintln!("failed to copy");
+            };
+        }
 
-        let mut file = File::create(r"public/foo.txt").await?;
-        // 读取器
-        // let buffer = "Hello World".as_bytes();
-        let buffer = b"Hello World";
-        file.write_all(buffer).await.unwrap();
-    }
-
-    {
-        let mut file = File::create(r"public/foo.txt").await?;
-        // 读取器
-        let mut buffer = "Hello World".as_bytes();
-
-        io::copy(&mut buffer, &mut file).await.unwrap();
+        loop {
+            let (stream, addr) = listener.accept().await?;
+            tokio::spawn(async move { process(stream).await });
+        }
     }
 
     Ok(())
